6254591ac24d7311b30bb9c57f03a30cba5daaac
gimhanul <gimhanuly@gmail.com>
2023-04-12 23:01:19 +0900

    initialize

---
+++ src/main/java/com/bamdoliro/maru/MaruApplication.java
@@ -0,0 +1,13 @@
+package com.bamdoliro.maru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaruApplication.class, args);
+	}
+
+}
