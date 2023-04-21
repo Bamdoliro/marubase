8b57ba58ea9d7e570f656a77379bd8e2bbd36f5c
gimhanul <gimhanuly@gmail.com>
2023-04-21 14:40:18 +0900

    TEST :: user error case (#3)
    - user error test case 추가
    - exception handler 추가

--- src/main/java/com/bamdoliro/maru/presentation/user/UserController.java
+++ src/main/java/com/bamdoliro/maru/presentation/user/UserController.java
@@ -5,6 +5,7 @@ import com.bamdoliro.maru.presentation.user.dto.request.SignUpUserRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/bamdoliro/maru/shared/error/GlobalExceptionHandler.java
+++ src/main/java/com/bamdoliro/maru/shared/error/GlobalExceptionHandler.java
@@ -1,13 +1,34 @@
 package com.bamdoliro.maru.shared.error;
 
-import lombok.extern.slf4j.Slf4j;
+import com.bamdoliro.maru.shared.response.ErrorResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@Slf4j
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(BindException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ErrorResponse> bindException(BindException e) {
+        Map<String, String> errorMap = new HashMap<>();
+
+        for (FieldError error : e.getFieldErrors()) {
+            errorMap.put(error.getField(), error.getDefaultMessage());
+        }
+
+        return ResponseEntity
+                .status(GlobalErrorProperty.BAD_REQUEST.getStatus())
+                .body(new ErrorResponse(GlobalErrorProperty.BAD_REQUEST, errorMap));
+    }
 
     @ExceptionHandler(MaruException.class)
     public ResponseEntity<ErrorResponse> handleMaruException(MaruException e) {
