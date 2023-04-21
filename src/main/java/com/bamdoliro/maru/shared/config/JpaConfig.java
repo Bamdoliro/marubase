a0edf4073641b8fea8be5c38e92171c8cc98a732
gimhanul <gimhanuly@gmail.com>
2023-04-21 09:53:55 +0900

    ADD :: BaseTimeEntity (#3)

---
+++ src/main/java/com/bamdoliro/maru/shared/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.bamdoliro.maru.shared.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+}
---
+++ src/main/java/com/bamdoliro/maru/shared/entity/BaseTimeEntity.java
@@ -0,0 +1,25 @@
+package com.bamdoliro.maru.shared.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @Column(nullable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
