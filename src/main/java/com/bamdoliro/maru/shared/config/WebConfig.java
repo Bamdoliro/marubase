61d3df3488ffce4f2c9969a81d503f6c734067bb
gimhanul <gimhanuly@gmail.com>
2023-04-21 09:55:08 +0900

    ADD :: cors config (#3)

---
+++ src/main/java/com/bamdoliro/maru/shared/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.bamdoliro.maru.shared.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://localhost:3000",
+                        "http://localhost:3000"
+                )
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.PATCH.name()
+                );
+    }
+}
