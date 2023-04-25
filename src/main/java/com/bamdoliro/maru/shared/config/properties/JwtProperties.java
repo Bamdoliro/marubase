310e4267df643dd2683d6da52c499d51bde72533
gimhanul <gimhanuly@gmail.com>
2023-04-25 23:50:35 +0900

    ADD :: configuration properties setting

--- src/main/java/com/bamdoliro/maru/shared/config/properties/JwtProperties.java
+++ src/main/java/com/bamdoliro/maru/shared/config/properties/JwtProperties.java
@@ -5,15 +5,12 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@Setter
 @Getter
-@Configuration
 @ConfigurationProperties("jwt")
 public class JwtProperties {
 
     private Long accessExpirationTime;
     private Long refreshExpirationTime;
     private String prefix;
-    private String header;
     private String secretKey;
 }
