29e281ebb36c734738eb88b42ecc784965ef85fc
gimhanul <gimhanuly@gmail.com>
2023-04-26 16:32:58 +0900

    TEST :: token service, user facade

---
+++ src/test/java/com/bamdoliro/maru/domain/auth/service/TokenServiceTest.java
@@ -0,0 +1,66 @@
+package com.bamdoliro.maru.domain.auth.service;
+
+import com.bamdoliro.maru.domain.auth.domain.Token;
+import com.bamdoliro.maru.domain.user.domain.User;
+import com.bamdoliro.maru.infrastructure.persistence.auth.TokenRepository;
+import com.bamdoliro.maru.shared.config.properties.JwtProperties;
+import com.bamdoliro.maru.shared.fixture.UserFixture;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class TokenServiceTest {
+
+    @InjectMocks
+    private TokenService tokenService;
+
+    @Mock
+    private TokenRepository tokenRepository;
+
+    @Mock
+    private JwtProperties jwtProperties;
+
+    @Test
+    void 액세스_토큰을_생성한다() {
+        // given
+        User user = UserFixture.createUser();
+        given(jwtProperties.getAccessExpirationTime()).willReturn(1000L);
+        given(jwtProperties.getSecretKey()).willReturn("탑시크릿정말정말탑시크릿진짜옝용찐찐찐찐찐이야");
+
+        // when
+        String accessToken = tokenService.generateAccessToken(user.getEmail());
+
+        // then
+        verify(jwtProperties, times(1)).getAccessExpirationTime();
+        verify(jwtProperties, times(1)).getSecretKey();
+        assertEquals(user.getEmail(), tokenService.getEmail(accessToken));
+        assertNotNull(accessToken);
+    }
+
+    @Test
+    void 리프레시_토큰을_생성한다() {
+        // given
+        User user = UserFixture.createUser();
+        given(jwtProperties.getRefreshExpirationTime()).willReturn(1000L);
+        given(jwtProperties.getSecretKey()).willReturn("탑시크릿정말정말탑시크릿진짜옝용찐찐찐찐찐이야");
+
+        // when
+        String refreshToken = tokenService.generateRefreshToken(user.getEmail());
+
+        // then
+        verify(jwtProperties, times(1)).getRefreshExpirationTime();
+        verify(jwtProperties, times(1)).getSecretKey();
+        verify(tokenRepository, times(1)).save(any(Token.class));
+        assertEquals(user.getEmail(), tokenService.getEmail(refreshToken));
+        assertNotNull(refreshToken);
+    }
+}
---
+++ src/test/java/com/bamdoliro/maru/domain/user/service/UserFacadeTest.java
@@ -0,0 +1,49 @@
+package com.bamdoliro.maru.domain.user.service;
+
+import com.bamdoliro.maru.domain.user.domain.User;
+import com.bamdoliro.maru.domain.user.exception.UserNotFoundException;
+import com.bamdoliro.maru.infrastructure.persistence.user.UserRepository;
+import com.bamdoliro.maru.shared.fixture.UserFixture;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class UserFacadeTest {
+
+    @InjectMocks
+    private UserFacade userFacade;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Test
+    void 유저를_가져온다() {
+        // given
+        User user = UserFixture.createUser();
+        given(userRepository.findByEmail(user.getEmail())).willReturn(Optional.of(user));
+
+        // when
+        User foundUser = userFacade.getUser(user.getEmail());
+
+        // then
+        assertEquals(user.getEmail(), foundUser.getEmail());
+    }
+
+    @Test
+    void 존재하지_않는_유저일_때_에러가_발생한다() {
+        // given
+        given(userRepository.findByEmail(anyString())).willReturn(Optional.empty());
+
+        // when and then
+        assertThrows(UserNotFoundException.class, () -> userFacade.getUser("저는없는데요"));
+    }
+}
