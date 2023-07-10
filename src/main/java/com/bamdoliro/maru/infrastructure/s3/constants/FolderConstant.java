7022013679292f562c406ce38fac3642377fedf5
gimhanul <gimhanuly@gmail.com>
2023-07-10 13:26:06 +0900

    feat(#30): upload identification picture
    - upload usecase
    - upload controller

---
+++ src/main/java/com/bamdoliro/maru/infrastructure/s3/constants/FolderConstant.java
@@ -0,0 +1,10 @@
+package com.bamdoliro.maru.infrastructure.s3.constants;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class FolderConstant {
+    
+    public static final String IDENTIFICATION_PICTURE = "identification-picture";
+    public static final String FORM = "form";
+}
