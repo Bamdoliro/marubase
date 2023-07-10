77cb76515dad6c8ee743c149788f28fbc4b334eb
gimhanul <gimhanuly@gmail.com>
2023-07-10 10:25:31 +0900

    refactor(#29): delet e.printStackTrace()
    - e.printStackTrace 삭제
    - Exception catch할 때 원래 Exception으로 했는데 세분화 함

--- src/main/java/com/bamdoliro/maru/infrastructure/s3/UploadFileService.java
+++ src/main/java/com/bamdoliro/maru/infrastructure/s3/UploadFileService.java
@@ -1,5 +1,6 @@
 package com.bamdoliro.maru.infrastructure.s3;
 
+import com.amazonaws.SdkClientException;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
@@ -11,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.UUID;
 
 @Service
@@ -32,8 +34,7 @@ public class UploadFileService {
                     getObjectMetadata(file)
             );
             amazonS3Client.putObject(request.withCannedAcl(CannedAccessControlList.PublicRead));
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (SdkClientException | IOException e) {
             throw new FailedToSaveException();
         }
 
