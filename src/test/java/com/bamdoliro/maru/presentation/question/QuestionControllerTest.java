dd3775671ff2ef467f828f35833ba41fe819f926
gimhanul <gimhanuly@gmail.com>
2023-08-01 17:21:30 +0900

    test(#23): query question usecase, controller
    - query question document

--- src/test/java/com/bamdoliro/maru/presentation/question/QuestionControllerTest.java
+++ src/test/java/com/bamdoliro/maru/presentation/question/QuestionControllerTest.java
@@ -5,7 +5,9 @@ import com.bamdoliro.maru.domain.question.exception.QuestionNotFoundException;
 import com.bamdoliro.maru.domain.user.domain.User;
 import com.bamdoliro.maru.presentation.question.dto.request.CreateQuestionRequest;
 import com.bamdoliro.maru.presentation.question.dto.request.UpdateQuestionRequest;
+import com.bamdoliro.maru.presentation.question.dto.response.QuestionResponse;
 import com.bamdoliro.maru.shared.fixture.AuthFixture;
+import com.bamdoliro.maru.shared.fixture.QuestionFixture;
 import com.bamdoliro.maru.shared.fixture.UserFixture;
 import com.bamdoliro.maru.shared.util.RestDocsTestSupport;
 import org.junit.jupiter.api.Test;
@@ -14,20 +16,25 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 
+import java.util.List;
+
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.willDoNothing;
 import static org.mockito.BDDMockito.willThrow;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class QuestionControllerTest extends RestDocsTestSupport {
@@ -47,7 +54,9 @@ class QuestionControllerTest extends RestDocsTestSupport {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(toJson(request))
                 )
+
                 .andExpect(status().isCreated())
+
                 .andDo(restDocs.document(
                         requestHeaders(
                                 headerWithName(HttpHeaders.AUTHORIZATION)
@@ -73,22 +82,23 @@ class QuestionControllerTest extends RestDocsTestSupport {
         UpdateQuestionRequest request = new UpdateQuestionRequest("이거 맞나", "아님 말고...", QuestionCategory.TOP_QUESTION);
 
         User user = UserFixture.createAdminUser();
-        given(jwtProperties.getPrefix()).willReturn("Bearer");
-        given(tokenService.getUser(anyString())).willReturn(user);
-        
+        given(authenticationArgumentResolver.supportsParameter(any(MethodParameter.class))).willReturn(true);
+        given(authenticationArgumentResolver.resolveArgument(any(), any(), any(), any())).willReturn(user);
 
-        long id = 1;
-        mockMvc.perform(put("/question/{id}", id)
+        mockMvc.perform(put("/question/{id}", 1)
                         .header(HttpHeaders.AUTHORIZATION, AuthFixture.createAuthHeader())
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(toJson(request)))
+
+                .andExpect(status().isNoContent())
+
                 .andDo(restDocs.document(
                         requestHeaders(
                                 headerWithName(HttpHeaders.AUTHORIZATION)
                                         .description("Bearer token")
                         ),
-                        pathParameters(parameterWithName("id").description("질문 ID")),
+                        pathParameters(parameterWithName("id").description("자주 묻는 질문 id")),
                         requestFields(
                                 fieldWithPath("title")
                                         .type(JsonFieldType.STRING)
@@ -102,6 +112,7 @@ class QuestionControllerTest extends RestDocsTestSupport {
                         )
                 ));
     }
+
     @Test
     void 자주묻는질문을_수정할_때_자주묻는질문이_없으면_에러가_발생한다() throws Exception {
         Long id = 1L;
@@ -117,10 +128,35 @@ class QuestionControllerTest extends RestDocsTestSupport {
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(toJson(request)))
+
                 .andExpect(status().isNotFound())
+
                 .andDo(restDocs.document());
     }
 
+    @Test
+    void 자주묻는질문을_불러온다() throws Exception {
+        List<QuestionResponse> response = List.of(
+                QuestionFixture.createQuestionResponse(),
+                QuestionFixture.createQuestionResponse(),
+                QuestionFixture.createQuestionResponse()
+        );
+        given(queryQuestionListUseCase.execute(QuestionCategory.TOP_QUESTION)).willReturn(response);
 
+        mockMvc.perform(get("/question")
+                        .param("category", QuestionCategory.TOP_QUESTION.name())
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON))
 
+                .andExpect(status().isOk())
+
+                .andDo(restDocs.document(
+                        queryParameters(
+                                parameterWithName("category")
+                                        .description("카테고리")
+                        )
+                ));
+
+        verify(queryQuestionListUseCase, times(1)).execute(QuestionCategory.TOP_QUESTION);
+    }
 }
--- src/test/java/com/bamdoliro/maru/shared/fixture/QuestionFixture.java
+++ src/test/java/com/bamdoliro/maru/shared/fixture/QuestionFixture.java
@@ -2,6 +2,7 @@ package com.bamdoliro.maru.shared.fixture;
 
 import com.bamdoliro.maru.domain.question.domain.Question;
 import com.bamdoliro.maru.domain.question.domain.type.QuestionCategory;
+import com.bamdoliro.maru.presentation.question.dto.response.QuestionResponse;
 
 public class QuestionFixture {
 
@@ -9,4 +10,7 @@ public class QuestionFixture {
         return new Question("급식 맛있나요?", "오늘 저녁 돼지국밥!!!!!!", QuestionCategory.TOP_QUESTION);
     }
 
+    public static QuestionResponse createQuestionResponse() {
+        return new QuestionResponse(createQuestion());
+    }
 }
