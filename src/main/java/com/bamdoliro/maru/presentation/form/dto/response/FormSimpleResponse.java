6e5d09ded0d14af4295a64f6c29e9fb46bea7a97
gimhanul <gimhanuly@gmail.com>
2023-08-21 20:19:29 +0900

    refactor: add fields to form simple response

--- src/main/java/com/bamdoliro/maru/presentation/form/dto/response/FormSimpleResponse.java
+++ src/main/java/com/bamdoliro/maru/presentation/form/dto/response/FormSimpleResponse.java
@@ -4,8 +4,11 @@ package com.bamdoliro.maru.presentation.form.dto.response;
 import com.bamdoliro.maru.domain.form.domain.Form;
 import com.bamdoliro.maru.domain.form.domain.type.FormStatus;
 import com.bamdoliro.maru.domain.form.domain.type.FormType;
+import com.bamdoliro.maru.domain.form.domain.type.GraduationType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.time.LocalDate;
 
 @Getter
 @AllArgsConstructor
@@ -13,13 +16,19 @@ public class FormSimpleResponse {
 
     private Long id;
     private String name;
-    private FormStatus status;
+    private LocalDate birthday;
+    private GraduationType graduationType;
+    private String school;
+    private String status;
     private FormType type;
 
     public FormSimpleResponse(Form form) {
         this.id = form.getId();
         this.name = form.getApplicant().getName();
-        this.status = form.getStatus();
+        this.birthday = form.getApplicant().getBirthday();
+        this.graduationType = form.getEducation().getGraduationType();
+        this.school = form.getEducation().getSchool().getName();
+        this.status = form.getStatus().getDescription();
         this.type = form.getType();
     }
 }
