5f9a856f67308b929b180ce593541c95e787731a
gimhanul <gimhanuly@gmail.com>
2023-09-21 10:24:53 +0900

    refactor: change 400 message

--- src/main/java/com/bamdoliro/maru/shared/error/GlobalErrorProperty.java
+++ src/main/java/com/bamdoliro/maru/shared/error/GlobalErrorProperty.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum GlobalErrorProperty implements ErrorProperty {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에 알 수 없는 에러가 발생했습니다."),
-    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다.");
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다. 다시 한 번 확인해 주세요.");
 
     private final HttpStatus status;
     private final String message;
