86dac3beb07f8bca543c6fe834afc61088f7c94e
jyj1289 <jin37091289@gmail.com>
2024-05-01 19:54:15 +0900

    feat : MessageController - 메시지를 그룹별로 보낼 수 있는 메세지 컨트롤러를 만들었습니다.

---
+++ src/main/java/com/bamdoliro/maru/presentation/message/MessageController.java
@@ -0,0 +1,28 @@
+package com.bamdoliro.maru.presentation.message;
+
+import com.bamdoliro.maru.domain.user.domain.User;
+import com.bamdoliro.maru.infrastructure.message.SendMessageService;
+import com.bamdoliro.maru.presentation.message.dto.request.SendMessageRequest;
+import com.bamdoliro.maru.shared.auth.AuthenticationPrincipal;
+import com.bamdoliro.maru.shared.auth.Authority;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RequestMapping("/message")
+@RestController
+public class MessageController {
+
+    private final SendMessageService messageService;
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PostMapping
+    public void sendMessage(
+            @AuthenticationPrincipal(authority = Authority.ADMIN) User user,
+            @RequestBody @Valid SendMessageRequest request
+            ){
+        messageService.execute(request);
+    }
+}
