9d04ef81db8d52b3c35a11cee15f23627a15956c
jyj1289 <jin37091289@gmail.com>
2024-05-01 19:57:34 +0900

    feat(#75):send message by group service - 메시지를 그룹별로 보낼 수 있는 메세시를 보낼 수 있는 서비스를 만들었습니다.

--- src/main/java/com/bamdoliro/maru/infrastructure/message/SendMessageService.java
+++ src/main/java/com/bamdoliro/maru/infrastructure/message/SendMessageService.java
@@ -1,6 +1,9 @@
 package com.bamdoliro.maru.infrastructure.message;
 
+import com.bamdoliro.maru.domain.form.domain.Form;
 import com.bamdoliro.maru.infrastructure.message.exception.FailedToSendException;
+import com.bamdoliro.maru.infrastructure.persistence.form.FormRepository;
+import com.bamdoliro.maru.presentation.message.dto.request.SendMessageRequest;
 import com.bamdoliro.maru.shared.config.properties.MessageProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +12,9 @@ import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
 import net.nurigo.sdk.message.service.DefaultMessageService;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j
 @RequiredArgsConstructor
 @Service
@@ -16,6 +22,7 @@ public class SendMessageService {
 
     private final MessageProperties messageProperties;
     private final DefaultMessageService messageService;
+    private final FormRepository formRepository;
 
     public void execute(String to, String text) {
         Message message = createMessage(to, text);
@@ -26,6 +33,24 @@ public class SendMessageService {
         Message message = createMessage(to, text);
         message.setSubject(title);
         sendOneMessage(message);
+    }
+
+    public void execute(SendMessageRequest request){
+        List<Form> formList = formRepository.findByStatus(request.getStatus());
+        List<String> phoneNumberList = new ArrayList<>();
+        List<Message> messageList = new ArrayList<>();
+
+        for (Form form : formList){
+            phoneNumberList.add(form.getUser().getPhoneNumber());
+        }
+
+        for(String phoneNumber : phoneNumberList){
+            Message message = createMessage(phoneNumber, request.getText());
+            message.setSubject(request.getTitle());
+            messageList.add(message);
+        }
+
+        sendManyMessage(messageList);
     }
 
     private Message createMessage(String to, String text) {
@@ -43,6 +68,16 @@ public class SendMessageService {
             );
         } catch (Exception e) {
             log.error(e.getMessage());
+            throw new FailedToSendException();
+        }
+    }
+
+    private void sendManyMessage(List<Message> messageList){
+        try{
+            messageService.send(messageList, false, false);
+        } catch (Exception e){
+            log.error(e.getMessage());
+            e.printStackTrace();
             throw new FailedToSendException();
         }
     }
