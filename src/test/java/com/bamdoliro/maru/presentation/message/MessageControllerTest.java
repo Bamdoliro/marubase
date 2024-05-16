411cad34737b18f082946fd490a29f0eb18eccbc
jyj1289 <jin37091289@gmail.com>
2024-05-16 17:42:15 +0900

    test(#75) : 엔드포인트 변경 및 BDDMockito 메서드로 변경 - BDD관점으로 보기 위해서 BDDMockito메서드로 변경했어요. - UseCaseTest에서 status별로 테스트하기 위해서 컨트롤러 테스트를 수정했어요.

--- src/test/java/com/bamdoliro/maru/presentation/message/MessageControllerTest.java
+++ src/test/java/com/bamdoliro/maru/presentation/message/MessageControllerTest.java
@@ -1,30 +1,19 @@
 package com.bamdoliro.maru.presentation.message;
 
-import com.bamdoliro.maru.domain.form.domain.Form;
 import com.bamdoliro.maru.domain.form.domain.type.FormStatus;
-import com.bamdoliro.maru.domain.form.domain.type.FormType;
 import com.bamdoliro.maru.domain.user.domain.User;
-import com.bamdoliro.maru.infrastructure.persistence.form.FormRepository;
 import com.bamdoliro.maru.presentation.message.dto.request.SendMessageRequest;
 import com.bamdoliro.maru.shared.fixture.AuthFixture;
-import com.bamdoliro.maru.shared.fixture.FormFixture;
 import com.bamdoliro.maru.shared.fixture.UserFixture;
 import com.bamdoliro.maru.shared.util.RestDocsTestSupport;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 
-import java.util.Arrays;
-import java.util.List;
-
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doThrow;
+import static org.mockito.BDDMockito.*;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
@@ -34,52 +23,30 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 public class MessageControllerTest extends RestDocsTestSupport {
 
-    @MockBean
-    private FormRepository formRepository;
-
-    @BeforeEach
-    void setup() {
-        Form form = FormFixture.createForm(FormType.REGULAR);
-        form.receive();
-        List<Form> forms = Arrays.asList(form);
-
-        given(formRepository.findByStatus(FormStatus.RECEIVED)).willReturn(forms);
-    }
-
     @Test
-    void 원서가_접수된_학생들에게_메시지를_보낸다() throws Exception {
-
-        User user = UserFixture.createAdminUser();
+    void 조회할_상태의_원서를_가진_학생들에게() throws Exception {
+        User user = UserFixture.createUser();
         given(authenticationArgumentResolver.supportsParameter(any(MethodParameter.class))).willReturn(true);
         given(authenticationArgumentResolver.resolveArgument(any(), any(), any(), any())).willReturn(user);
 
-        SendMessageRequest request = new SendMessageRequest("부산소마고 공지사항", "배고파요..", FormStatus.RECEIVED);
-        doNothing().when(sendMessageUseCase).execute(any(SendMessageRequest.class));
+        SendMessageRequest request = new SendMessageRequest("부산소마고 공지사항", "테스트임니다...", FormStatus.FINAL_SUBMITTED);
+        willDoNothing().given(sendMessageUseCase).execute(request);
 
-        mockMvc.perform(post("/message")
+        mockMvc.perform(post("/message/status")
                         .header(HttpHeaders.AUTHORIZATION, AuthFixture.createAuthHeader())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(toJson(request)))
                 .andExpect(status().isNoContent())
-
                 .andDo(restDocs.document(
                         requestHeaders(
-                                headerWithName(HttpHeaders.AUTHORIZATION)
-                                        .description("Bearer token")
+                                headerWithName(HttpHeaders.AUTHORIZATION).description("Bearer token")
                         ),
                         requestFields(
-                                fieldWithPath("title")
-                                        .type(JsonFieldType.STRING)
-                                        .description("문자 메시지 제목"),
-                                fieldWithPath("text")
-                                        .type(JsonFieldType.STRING)
-                                        .description("문자 메시지 내용"),
-                                fieldWithPath("status")
-                                        .type(JsonFieldType.STRING)
-                                        .description("메시지를 보낼 원서의 상태")
+                                fieldWithPath("title").type(JsonFieldType.STRING).description("문자 메시지 제목"),
+                                fieldWithPath("text").type(JsonFieldType.STRING).description("문자 메시지 내용"),
+                                fieldWithPath("status").type(JsonFieldType.STRING).description("메시지를 보낼 원서의 상태")
                         )
                 ));
-
     }
 
     @Test
@@ -90,9 +57,9 @@ public class MessageControllerTest extends RestDocsTestSupport {
 
         SendMessageRequest request = new SendMessageRequest("부산소마고 공지사항", "배고파요..", FormStatus.FINAL_SUBMITTED);
 
-        doThrow(new RuntimeException("원서를 찾을 수 없음")).when(sendMessageUseCase).execute(any(SendMessageRequest.class));
+        willThrow(new RuntimeException("원서를 찾을 수 없음")).given(sendMessageUseCase).execute(any(SendMessageRequest.class));
 
-        mockMvc.perform(post("/message")
+        mockMvc.perform(post("/message/status")
                         .header(HttpHeaders.AUTHORIZATION, AuthFixture.createAuthHeader())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(toJson(request)))
