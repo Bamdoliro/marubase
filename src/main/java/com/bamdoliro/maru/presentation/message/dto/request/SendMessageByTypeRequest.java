68b8f9a7226f9c2c5bada53fb82176fbd9f9d8da
jyj1289 <jin37091289@gmail.com>
2024-05-30 19:22:57 +0900

    feat(#81) : SendMessageByTypeRequest 추가 - SendMessageByTypeRequest DTO를 추가했어요.

---
+++ src/main/java/com/bamdoliro/maru/presentation/message/dto/request/SendMessageByTypeRequest.java
@@ -0,0 +1,27 @@
+package com.bamdoliro.maru.presentation.message.dto.request;
+
+import com.bamdoliro.maru.domain.form.domain.type.FormType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SendMessageByTypeRequest {
+
+    @NotBlank(message = "필수값입니다.")
+    private String title;
+
+    @NotBlank(message = "필수값입니다.")
+    private String text;
+
+    @NotNull(message = "필수값입니다.")
+    private FormType formType;
+
+    @NotNull(message = "필수값입니다.")
+    private boolean isChangeToRegular;
+}
+
