7d8f19e26a8ea5784cce5c3862c4a306baca6996
jyj1289 <jin37091289@gmail.com>
2024-05-31 17:14:32 +0900

    feat(#81) : messageControllerTest 추가 - /message/type 테스트를 추가했어요.

--- src/test/java/com/bamdoliro/maru/presentation/message/MessageControllerTest.java
+++ src/test/java/com/bamdoliro/maru/presentation/message/MessageControllerTest.java
@@ -1,8 +1,10 @@
 package com.bamdoliro.maru.presentation.message;
 
 import com.bamdoliro.maru.domain.form.domain.type.FormStatus;
+import com.bamdoliro.maru.domain.form.domain.type.FormType;
 import com.bamdoliro.maru.domain.user.domain.User;
 import com.bamdoliro.maru.presentation.message.dto.request.SendMessageByStatusRequest;
+import com.bamdoliro.maru.presentation.message.dto.request.SendMessageByTypeRequest;
 import com.bamdoliro.maru.shared.fixture.AuthFixture;
 import com.bamdoliro.maru.shared.fixture.UserFixture;
 import com.bamdoliro.maru.shared.util.RestDocsTestSupport;
@@ -72,6 +74,97 @@ public class MessageControllerTest extends RestDocsTestSupport {
                                 fieldWithPath("title").type(JsonFieldType.STRING).description("문자 메시지 제목"),
                                 fieldWithPath("text").type(JsonFieldType.STRING).description("문자 메시지 내용"),
                                 fieldWithPath("status").type(JsonFieldType.STRING).description("메시지를 보낼 원서의 상태")
+                        )
+                ));
+    }
+
+    @Test
+    void 마이스터전형과_마이스터에서_일반전형으로_바뀐_합격자를_제외한_1차_합격자들에게_메시지를_보낸다() throws Exception {
+        User user = UserFixture.createAdminUser();
+        given(authenticationArgumentResolver.supportsParameter(any(MethodParameter.class))).willReturn(true);
+        given(authenticationArgumentResolver.resolveArgument(any(), any(), any(), any())).willReturn(user);
+
+        SendMessageByTypeRequest request = new SendMessageByTypeRequest("부산소마고 공지사항", "추카추카", FormType.REGULAR, false);
+        willDoNothing().given(sendMessageUseCase).execute(request);
+
+        mockMvc.perform(post("/message/type")
+                        .header(HttpHeaders.AUTHORIZATION, AuthFixture.createAuthHeader())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(toJson(request)))
+                .andExpect(status().isNoContent())
+                .andDo(restDocs.document(
+                        requestHeaders(
+                                headerWithName(HttpHeaders.AUTHORIZATION).description("Bearer token")
+                        ),
+                        requestFields(
+                                fieldWithPath("title").type(JsonFieldType.STRING).description("문자 메시지 제목"),
+                                fieldWithPath("text").type(JsonFieldType.STRING).description("문자 메시지 내용"),
+                                fieldWithPath("formType").type(JsonFieldType.STRING).description("메시지를 보낼 원서 전형(MEISTER_TALENT, REGULAR)"),
+                                fieldWithPath("isChangeToRegular").type(JsonFieldType.BOOLEAN).description("만약 마이스터 -> 일반 전형이면 true 아니면 false")
+                        )
+                ));
+    }
+
+    @Test
+    void 마이스터_전형_1차_합격자들에게_메시지를_보낸다() throws Exception {
+        User user = UserFixture.createAdminUser();
+        given(authenticationArgumentResolver.supportsParameter(any(MethodParameter.class))).willReturn(true);
+        given(authenticationArgumentResolver.resolveArgument(any(), any(), any(), any())).willReturn(user);
+
+        SendMessageByTypeRequest request = new SendMessageByTypeRequest("부산소마고 공지사항", "추카추카", FormType.MEISTER_TALENT, false);
+        willDoNothing().given(sendMessageUseCase).execute(request);
+
+        mockMvc.perform(post("/message/type")
+                        .header(HttpHeaders.AUTHORIZATION, AuthFixture.createAuthHeader())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(toJson(request)))
+                .andExpect(status().isNoContent())
+
+                .andDo(restDocs.document());
+    }
+
+    @Test
+    void 마이스터전형에서_일반전형으로_바뀐_1차_합격자들에게_메시지를_보낸다() throws Exception {
+        User user = UserFixture.createAdminUser();
+        given(authenticationArgumentResolver.supportsParameter(any(MethodParameter.class))).willReturn(true);
+        given(authenticationArgumentResolver.resolveArgument(any(), any(), any(), any())).willReturn(user);
+
+        SendMessageByTypeRequest request = new SendMessageByTypeRequest("부산소마고 공지사항", "추카추카", FormType.REGULAR, true);
+        willDoNothing().given(sendMessageUseCase).execute(request);
+
+        mockMvc.perform(post("/message/type")
+                        .header(HttpHeaders.AUTHORIZATION, AuthFixture.createAuthHeader())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(toJson(request)))
+                .andExpect(status().isNoContent())
+
+                .andDo(restDocs.document());
+    }
+
+    @Test
+    void 해당전형의_원서가_없다면_오류가_발생한다() throws Exception {
+        User user = UserFixture.createAdminUser();
+        given(authenticationArgumentResolver.supportsParameter(any(MethodParameter.class))).willReturn(true);
+        given(authenticationArgumentResolver.resolveArgument(any(), any(), any(), any())).willReturn(user);
+
+        SendMessageByTypeRequest request = new SendMessageByTypeRequest("부산소마고 공지사항", "오류가 발생하겠죠?", FormType.REGULAR, false);
+
+        willThrow(new RuntimeException("원서를 찾을 수 없음")).given(sendMessageUseCase).execute(any(SendMessageByTypeRequest.class));
+
+        mockMvc.perform(post("/message/type")
+                        .header(HttpHeaders.AUTHORIZATION, AuthFixture.createAuthHeader())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(toJson(request)))
+                .andExpect(status().isInternalServerError())
+                .andDo(restDocs.document(
+                        requestHeaders(
+                                headerWithName(HttpHeaders.AUTHORIZATION).description("Bearer token")
+                        ),
+                        requestFields(
+                                fieldWithPath("title").type(JsonFieldType.STRING).description("문자 메시지 제목"),
+                                fieldWithPath("text").type(JsonFieldType.STRING).description("문자 메시지 내용"),
+                                fieldWithPath("formType").type(JsonFieldType.STRING).description("메시지를 보낼 원서 전형(MEISTER_TALENT, REGULAR)"),
+                                fieldWithPath("isChangeToRegular").type(JsonFieldType.BOOLEAN).description("만약 마이스터 -> 일반 전형이면 true 아니면 false")
                         )
                 ));
     }
