a63b47c6eda9b227aebd2466ce6094943181ff42
jyj1289 <jin37091289@gmail.com>
2024-06-01 19:05:02 +0900

    feat(#83) : UpdatePasswordRequest 추가 - UpdatePasswordRequest를 만들었어요.

---
+++ src/main/java/com/bamdoliro/maru/presentation/user/dto/request/UpdatePasswordRequest.java
@@ -0,0 +1,23 @@
+package com.bamdoliro.maru.presentation.user.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdatePasswordRequest {
+
+    @NotBlank(message = "필수값입니다.")
+    @Size(min = 11, max = 11, message = "11글자여야 합니다.")
+    private String phoneNumber;
+
+    @NotBlank(message = "필수값입니다.")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$",
+            message = "비밀번호는 최소 하나의 문자, 숫자, 특수문자를 포함하며 8 글자 이상이어야 합니다.")
+    private String password;
+}
