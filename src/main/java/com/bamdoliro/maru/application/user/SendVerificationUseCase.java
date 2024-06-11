e6cf9d3d036782fbd4ef59dba94dd29b1729a088
jyj1289 <jin37091289@gmail.com>
2024-06-11 11:20:39 +0900

    feat(#83) : SendVerificationUseCase 코드 추가 - 회원가입과 비밀번호 변경 인증번호를 따로 두기 위해서 if문으로 구분했어요.

--- src/main/java/com/bamdoliro/maru/application/user/SendVerificationUseCase.java
+++ src/main/java/com/bamdoliro/maru/application/user/SendVerificationUseCase.java
@@ -1,8 +1,11 @@
 package com.bamdoliro.maru.application.user;
 
-import com.bamdoliro.maru.domain.user.domain.Verification;
+import com.bamdoliro.maru.domain.user.domain.SignUpVerification;
+import com.bamdoliro.maru.domain.user.domain.UpdatePasswordVerification;
+import com.bamdoliro.maru.domain.user.domain.type.VerificationType;
 import com.bamdoliro.maru.infrastructure.message.SendMessageService;
-import com.bamdoliro.maru.infrastructure.persistence.user.VerificationRepository;
+import com.bamdoliro.maru.infrastructure.persistence.user.SignUpVerificationRepository;
+import com.bamdoliro.maru.infrastructure.persistence.user.UpdatePasswordVerificationRepository;
 import com.bamdoliro.maru.presentation.user.dto.request.SendVerificationRequest;
 import com.bamdoliro.maru.shared.annotation.UseCase;
 import lombok.RequiredArgsConstructor;
@@ -12,20 +15,37 @@ import lombok.RequiredArgsConstructor;
 public class SendVerificationUseCase {
 
     private final SendMessageService sendMessageService;
-    private final VerificationRepository verificationRepository;
+    private final SignUpVerificationRepository signUpVerificationRepository;
+    private final UpdatePasswordVerificationRepository updatePasswordVerificationRepository;
 
     public void execute(SendVerificationRequest request) {
-        Verification verification = new Verification(request.getPhoneNumber());
-        String text = String.format(
-                "[부산소프트웨어마이스터고] 인증번호는 [%s]입니다.",
-                verification.getCode()
-        );
 
-        sendMessageService.execute(
-                request.getPhoneNumber(),
-                text
-        );
+        if (request.getType() == VerificationType.SIGNUP) {
+            SignUpVerification signUpVerification = new SignUpVerification(request.getPhoneNumber());
+            String text = String.format(
+                    "[부산소프트웨어마이스터고] 회원가입 인증번호는 [%s]입니다.",
+                    signUpVerification.getCode()
+            );
 
-        verificationRepository.save(verification);
+            sendMessageService.execute(
+                    request.getPhoneNumber(),
+                    text
+            );
+
+            signUpVerificationRepository.save(signUpVerification);
+        } else {
+            UpdatePasswordVerification updatePasswordVerification = new UpdatePasswordVerification(request.getPhoneNumber());
+            String text = String.format(
+                    "[부산소프트웨어마이스터고] 비밀번호 변경 인증번호는 [%s]입니다.",
+                    updatePasswordVerification.getCode()
+            );
+
+            sendMessageService.execute(
+                    request.getPhoneNumber(),
+                    text
+            );
+
+            updatePasswordVerificationRepository.save(updatePasswordVerification);
+        }
     }
 }
