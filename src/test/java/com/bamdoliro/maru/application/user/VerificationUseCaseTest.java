dd13b4605e18c333fe82288db8c22fac60f8a17d
jyj1289 <jin37091289@gmail.com>
2024-06-11 22:41:22 +0900

    feat(#83) : 회원가입 인증 번호 및 비밀번호 변경 인증 테스트 - 회원가입과 비밀번호 변경 인증번호를 구분하여 테스트 하는 메서드를 추가했어요.

--- src/test/java/com/bamdoliro/maru/application/user/VerificationUseCaseTest.java
+++ src/test/java/com/bamdoliro/maru/application/user/VerificationUseCaseTest.java
@@ -1,10 +1,12 @@
 package com.bamdoliro.maru.application.user;
 
 import com.bamdoliro.maru.domain.user.domain.SignUpVerification;
+import com.bamdoliro.maru.domain.user.domain.UpdatePasswordVerification;
 import com.bamdoliro.maru.domain.user.domain.type.VerificationType;
 import com.bamdoliro.maru.infrastructure.message.SendMessageService;
 import com.bamdoliro.maru.infrastructure.message.exception.FailedToSendException;
 import com.bamdoliro.maru.infrastructure.persistence.user.SignUpVerificationRepository;
+import com.bamdoliro.maru.infrastructure.persistence.user.UpdatePasswordVerificationRepository;
 import com.bamdoliro.maru.presentation.user.dto.request.SendVerificationRequest;
 import com.bamdoliro.maru.shared.fixture.UserFixture;
 import org.junit.jupiter.api.Test;
@@ -19,14 +21,11 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.willDoNothing;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
-class SendSignUpVerificationUseCaseTest {
+class VerificationUseCaseTest {
 
     @InjectMocks
     private SendVerificationUseCase sendVerificationUseCase;
@@ -37,6 +36,8 @@ class SendSignUpVerificationUseCaseTest {
     @Mock
     private SignUpVerificationRepository signUpVerificationRepository;
 
+    @Mock
+    private UpdatePasswordVerificationRepository updatePasswordVerificationRepository;
 
     @Test
     void 유저가_회원가입_전화번호_인증을_요청한다() {
@@ -67,5 +68,36 @@ class SendSignUpVerificationUseCaseTest {
 
         verify(sendMessageService, times(1)).execute(anyString(), anyString());
         verify(signUpVerificationRepository, never()).save(any(SignUpVerification.class));
+    }
+
+    @Test
+    void 유저가_비밀번호_변경_전화번호_인증을_요청한다() {
+        // given
+        UpdatePasswordVerification verification = UserFixture.createUpdatePasswordVerification(false);
+        willDoNothing().given(sendMessageService).execute(anyString(), anyString());
+        given(updatePasswordVerificationRepository.save(any(UpdatePasswordVerification.class))).willReturn(verification);
+
+        // when
+        sendVerificationUseCase.execute(new SendVerificationRequest(verification.getPhoneNumber(), VerificationType.UPDATE_PASSWORD));
+
+        // then
+        verify(sendMessageService, times(1)).execute(anyString(), anyString());
+        verify(updatePasswordVerificationRepository, times(1)).save(any(UpdatePasswordVerification.class));
+
+        assertNotNull(verification.getCode());
+    }
+
+    @Test
+    void 회원가입_비밀번호_변경전화번호_전송이_실패한다() {
+        // given
+        UpdatePasswordVerification verification = UserFixture.createUpdatePasswordVerification(false);
+        doThrow(new FailedToSendException()).when(sendMessageService).execute(anyString(), anyString());
+
+        // when and then
+        assertThrows(FailedToSendException.class,
+                () -> sendVerificationUseCase.execute(new SendVerificationRequest(verification.getPhoneNumber(), VerificationType.UPDATE_PASSWORD)));
+
+        verify(sendMessageService, times(1)).execute(anyString(), anyString());
+        verify(updatePasswordVerificationRepository, never()).save(any(UpdatePasswordVerification.class));
     }
 }
