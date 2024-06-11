ba6ab1e1d56d288e440613720e3f3eb24944ceec
jyj1289 <jin37091289@gmail.com>
2024-06-11 11:33:08 +0900

    feat(#83) : VerificationType 추가 - 회원가입과 비밀번호 변경을 구분하기 위해서 request에 type을 추가했어요.

--- src/main/java/com/bamdoliro/maru/presentation/user/dto/request/VerifyRequest.java
+++ src/main/java/com/bamdoliro/maru/presentation/user/dto/request/VerifyRequest.java
@@ -1,6 +1,8 @@
 package com.bamdoliro.maru.presentation.user.dto.request;
 
+import com.bamdoliro.maru.domain.user.domain.type.VerificationType;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -18,4 +20,7 @@ public class VerifyRequest {
     @NotBlank(message = "필수값입니다.")
     @Size(min = 6, max = 6, message = "6자여야 합니다.")
     private String code;
+
+    @NotNull(message = "필수값입니다.")
+    private VerificationType type;
 }
