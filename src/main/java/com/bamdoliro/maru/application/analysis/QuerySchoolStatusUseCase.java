43759663e891b35513bcc835c7f9b8b13be53963
cabbage16 <qocn16@gmail.com>
2024-06-13 12:02:25 +0900

    feat(#79): 지원자 출신 학교 현황 조회 - 어드민이 지원자들의 출신 학교 정보를 조회할 수 있어요.

---
+++ src/main/java/com/bamdoliro/maru/application/analysis/QuerySchoolStatusUseCase.java
@@ -0,0 +1,33 @@
+package com.bamdoliro.maru.application.analysis;
+
+import com.bamdoliro.maru.infrastructure.persistence.form.FormRepository;
+import com.bamdoliro.maru.presentation.analysis.dto.request.SchoolStatusRequest;
+import com.bamdoliro.maru.presentation.analysis.dto.response.SchoolStatusResponse;
+import com.bamdoliro.maru.shared.annotation.UseCase;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@UseCase
+public class QuerySchoolStatusUseCase {
+
+    private final FormRepository formRepository;
+
+    public List<SchoolStatusResponse> execute(SchoolStatusRequest request) {
+        if (request.getIsBusan()) {
+            String keyword = "부산광역시";
+            keyword += request.getGu() == null ? "" : (" " + request.getGu());
+
+            return formRepository.findSchoolByAddress(request.getStatusList(), keyword)
+                    .stream()
+                    .map(SchoolStatusResponse::new)
+                    .toList();
+        }
+
+        return formRepository.findNotBusanSchool(request.getStatusList())
+                .stream()
+                .map(SchoolStatusResponse::new)
+                .toList();
+    }
+}
