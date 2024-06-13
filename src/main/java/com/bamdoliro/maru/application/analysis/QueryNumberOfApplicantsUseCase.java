87ad37d8d677e1fac26586250137cd734a6e4190
cabbage16 <qocn16@gmail.com>
2024-06-13 11:58:53 +0900

    feat(#79): 전형별 지원자 수 조회 - 어드민이 전형별 지원자수를 조회할 수 있습니다

---
+++ src/main/java/com/bamdoliro/maru/application/analysis/QueryNumberOfApplicantsUseCase.java
@@ -0,0 +1,35 @@
+package com.bamdoliro.maru.application.analysis;
+
+import com.bamdoliro.maru.domain.form.domain.type.FormType;
+import com.bamdoliro.maru.infrastructure.persistence.form.FormRepository;
+import com.bamdoliro.maru.presentation.analysis.dto.response.NumberOfApplicantsResponse;
+import com.bamdoliro.maru.shared.annotation.UseCase;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@UseCase
+public class QueryNumberOfApplicantsUseCase {
+    private final FormRepository formRepository;
+
+    public List<NumberOfApplicantsResponse> execute() {
+        List<NumberOfApplicantsResponse> result = formRepository.findTypeAndCountGroupByType()
+                .stream()
+                .map(NumberOfApplicantsResponse::new)
+                .collect(Collectors.toList());
+
+        List<FormType> existingTypes = result.stream()
+                .map(NumberOfApplicantsResponse::getType)
+                .toList();
+
+        for (FormType formType: FormType.values()) {
+            if (!existingTypes.contains(formType)) {
+                result.add(new NumberOfApplicantsResponse(formType, 0L));
+            }
+        }
+
+        return result;
+    }
+}
