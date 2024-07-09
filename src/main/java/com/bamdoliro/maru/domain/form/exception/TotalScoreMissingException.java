8b2ca5ac72c0964eca08e087b6273cd22d0e3ca9
cabbage16 <qocn16@gmail.com>
2024-07-09 16:23:03 +0900

    refactor(#98): 사용되지 않는 import 문 제거

--- src/main/java/com/bamdoliro/maru/domain/form/exception/TotalScoreMissingException.java
+++ src/main/java/com/bamdoliro/maru/domain/form/exception/TotalScoreMissingException.java
@@ -1,7 +1,6 @@
 package com.bamdoliro.maru.domain.form.exception;
 
 import com.bamdoliro.maru.domain.form.exception.error.FormErrorProperty;
-import com.bamdoliro.maru.shared.error.ErrorProperty;
 import com.bamdoliro.maru.shared.error.MaruException;
 
 public class TotalScoreMissingException extends MaruException {
