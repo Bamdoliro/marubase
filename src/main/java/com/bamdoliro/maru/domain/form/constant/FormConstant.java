f0608a934e7732e459d3c680304ded476abb86ca
jyj1289 <jin37091289@gmail.com>
2024-07-10 12:03:16 +0900

    perf(#102): 최대, 최소 봉사시간 수정 - 2025학년도 입학요강에 따라 최대 봉사시간을 30시간, 최소 봉사시간을 15시간으로 변경했어요

--- src/main/java/com/bamdoliro/maru/domain/form/constant/FormConstant.java
+++ src/main/java/com/bamdoliro/maru/domain/form/constant/FormConstant.java
@@ -16,8 +16,8 @@ public class FormConstant {
     public static final int DEFAULT_VOLUNTEER_SCORE = 14;
     public static final int MIN_VOLUNTEER_SCORE = 0;
     public static final int MAX_VOLUNTEER_SCORE = 18;
-    public static final int MIN_VOLUNTEER_TIME = 7;
-    public static final int MAX_VOLUNTEER_TIME = 15;
+    public static final int MIN_VOLUNTEER_TIME = 15;
+    public static final int MAX_VOLUNTEER_TIME = 30;
 
     public static final int REGULAR_TYPE_DEFAULT_SCORE = 80;
     public static final int SPECIAL_TYPE_DEFAULT_SCORE = 48;
