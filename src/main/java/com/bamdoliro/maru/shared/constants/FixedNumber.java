7db154b15698e82c484e6ae8c37db4ad8bbe2134
jyj1289 <jin37091289@gmail.com>
2024-07-10 11:13:18 +0900

    perf(#102): 마이스터 인재전형과 사회통합전형의 비율 수정 - 2025학년도 입학요강에 따라 마이스터 인재전형을 25명에서 22명으로, 사회통합전형을 3명에서 6명으로 수정했어요.

--- src/main/java/com/bamdoliro/maru/shared/constants/FixedNumber.java
+++ src/main/java/com/bamdoliro/maru/shared/constants/FixedNumber.java
@@ -7,8 +7,8 @@ public class FixedNumber {
 
     public static final int TOTAL = 64;
     public static final int REGULAR = 36;
-    public static final int MEISTER_TALENT = 25;
-    public static final int SOCIAL_INTEGRATION = 3;
+    public static final int MEISTER_TALENT = 22;
+    public static final int SOCIAL_INTEGRATION = 6;
     public static final int NATIONAL_VETERANS_EDUCATION = 2;
     public static final int SPECIAL_ADMISSION = 1;
     public static final double MULTIPLE = 1.3;
