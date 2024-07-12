6f515b38be08db1a4c836f193746926d8a45a639
cabbage16 <qocn16@gmail.com>
2024-07-12 12:11:56 +0900

    test(#79): QueryGenderRatioUseCaseTest - QueryGenderRatioUseCaseTest 작성했습니다.

--- src/test/java/com/bamdoliro/maru/application/analysis/QueryGenderRatioUseCaseTest.java
+++ src/test/java/com/bamdoliro/maru/application/analysis/QueryGenderRatioUseCaseTest.java
@@ -1,11 +1,28 @@
 package com.bamdoliro.maru.application.analysis;
 
+import com.bamdoliro.maru.domain.form.domain.Form;
+import com.bamdoliro.maru.domain.form.domain.type.FormStatus;
+import com.bamdoliro.maru.domain.form.domain.type.FormType;
 import com.bamdoliro.maru.infrastructure.persistence.form.FormRepository;
+import com.bamdoliro.maru.presentation.analysis.dto.request.GenderRatioRequest;
+import com.bamdoliro.maru.presentation.analysis.dto.response.GenderRatioResponse;
+import com.bamdoliro.maru.presentation.analysis.dto.response.GradeDistributionResponse;
+import com.bamdoliro.maru.shared.fixture.FormFixture;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class QueryGenderRatioUseCaseTest {
@@ -17,7 +34,308 @@ class QueryGenderRatioUseCaseTest {
     private FormRepository formRepository;
 
     @Test
-    void 유저가_1차_합격자의_일반_전형_성비를_조회한다() {
+    void 어드민이_1차_합격자_중_일반_전형_성비를_조회한다() {
         // given
+        List<Form> formList = List.of(
+                FormFixture.createForm(FormType.REGULAR),
+                FormFixture.createMaleForm(FormType.REGULAR),
+                FormFixture.createMaleForm(FormType.REGULAR)
+        );
+        formList.forEach(Form::firstPass);
+
+        given(formRepository.findByCategory(FormType.Category.REGULAR)).willReturn(formList);
+        List<FormStatus> round = List.of(FormStatus.FIRST_PASSED, FormStatus.FAILED, FormStatus.PASSED);
+        GenderRatioRequest request = new GenderRatioRequest(round, FormType.Category.REGULAR);
+
+        // when
+        List<GenderRatioResponse> responseList = queryGenderRatioUseCase.execute(request);
+
+        // then
+        assertEquals(responseList.stream()
+                .mapToLong(response -> response.getBusanFemale() + response.getOtherLocationFemale())
+                .sum(), 1L);
+        assertEquals(responseList.stream()
+                .mapToLong(response -> response.getBusanMale() + response.getOtherLocationMale())
+                .sum(), 2L);
+        verify(formRepository, times(1)).findByCategory(FormType.Category.REGULAR);
+    }
+
+    @Test
+    void 어드민이_1차_합격자_중_특별_전형_성비를_조회한다() {
+        // given
+        List<Form> formList = List.of(
+                FormFixture.createForm(FormType.MEISTER_TALENT),
+                FormFixture.createForm(FormType.NATIONAL_BASIC_LIVING),
+                FormFixture.createMaleForm(FormType.MEISTER_TALENT),
+                FormFixture.createMaleForm(FormType.MULTICULTURAL),
+                FormFixture.createMaleForm(FormType.FARMING_AND_FISHING),
+                FormFixture.createMaleForm(FormType.ONE_PARENT),
+                FormFixture.createMaleForm(FormType.NEAR_POVERTY)
+        );
+        formList.forEach(Form::firstPass);
+
+        given(formRepository.findByCategory(FormType.Category.MEISTER_TALENT))
+                .willReturn(formList.stream()
+                        .filter(form -> form.getType().categoryEquals(FormType.Category.MEISTER_TALENT))
+                        .toList());
+        given(formRepository.findByCategory(FormType.Category.SOCIAL_INTEGRATION))
+                .willReturn(formList.stream()
+                        .filter(form -> form.getType().categoryEquals(FormType.Category.SOCIAL_INTEGRATION))
+                        .toList());
+        List<FormStatus> round = List.of(FormStatus.FIRST_PASSED, FormStatus.FAILED, FormStatus.PASSED);
+        GenderRatioRequest request = new GenderRatioRequest(round, FormType.Category.SPECIAL);
+
+        // when
+        List<GenderRatioResponse> responseList = queryGenderRatioUseCase.execute(request);
+
+        // then
+        assertEquals(responseList.stream()
+                .mapToLong(response -> response.getBusanMale() + response.getOtherLocationMale())
+                .sum(), 5L);
+        assertEquals(responseList.stream()
+                .mapToLong(response -> response.getBusanFemale() + response.getOtherLocationFemale())
+                .sum(), 2L);
+        verify(formRepository, times(1)).findByCategory(FormType.Category.MEISTER_TALENT);
+        verify(formRepository, times(1)).findByCategory(FormType.Category.SOCIAL_INTEGRATION);
+    }
+
+    @Test
+    void 어드민이_1차_합격자_중_정원_외_전형_성비를_조회한다() {
+        // given
+        List<Form> formList = List.of(
+                FormFixture.createForm(FormType.NATIONAL_VETERANS_EDUCATION),
+                FormFixture.createForm(FormType.SPECIAL_ADMISSION),
+                FormFixture.createMaleForm(FormType.NATIONAL_VETERANS_EDUCATION),
+                FormFixture.createMaleForm(FormType.SPECIAL_ADMISSION)
+        );
+        formList.forEach(Form::firstPass);
+
+        given(formRepository.findByCategory(FormType.Category.NATIONAL_VETERANS_EDUCATION))
+                .willReturn(formList.stream()
+                        .filter(form -> form.getType().categoryEquals(FormType.Category.NATIONAL_VETERANS_EDUCATION))
+                        .toList());
+        given(formRepository.findByCategory(FormType.Category.SPECIAL_ADMISSION))
+                .willReturn(formList.stream()
+                        .filter(form -> form.getType().categoryEquals(FormType.Category.SPECIAL_ADMISSION))
+                        .toList());
+        List<FormStatus> round = List.of(FormStatus.FIRST_PASSED, FormStatus.FAILED, FormStatus.PASSED);
+        GenderRatioRequest request = new GenderRatioRequest(round, FormType.Category.SUPERNUMERARY);
+
+        // when
+        List<GenderRatioResponse> responseList = queryGenderRatioUseCase.execute(request);
+
+        // then
+        assertEquals(responseList.stream()
+                .mapToLong(response -> response.getBusanMale() + response.getOtherLocationMale())
+                .sum(), 2L);
+        assertEquals(responseList.stream()
+                .mapToLong(response -> response.getBusanFemale() + response.getOtherLocationFemale())
+                .sum(), 2L);
+        verify(formRepository, times(1)).findByCategory(FormType.Category.NATIONAL_VETERANS_EDUCATION);
+        verify(formRepository, times(1)).findByCategory(FormType.Category.SPECIAL_ADMISSION);
+    }
+
+    @Test
+    void 어드민이_2차_전형자_중_일반_전형_성비를_조회한다() {
+        // given
+        List<Form> formList = List.of(
+                FormFixture.createForm(FormType.REGULAR),
+                FormFixture.createMaleForm(FormType.REGULAR),
+                FormFixture.createMaleForm(FormType.REGULAR)
+        );
+        formList.forEach(Form::fail);
+
+        given(formRepository.findByCategory(FormType.Category.REGULAR)).willReturn(formList);
+        List<FormStatus> round = List.of(FormStatus.FAILED, FormStatus.PASSED);
+        GenderRatioRequest request = new GenderRatioRequest(round, FormType.Category.REGULAR);
+
+        // when
+        List<GenderRatioResponse> responseList = queryGenderRatioUseCase.execute(request);
+
+        // then
+        assertEquals(responseList.stream()
+                .mapToLong(response -> response.getBusanFemale() + response.getOtherLocationFemale())
+                .sum(), 1L);
+        assertEquals(responseList.stream()
+                .mapToLong(response -> response.getBusanMale() + response.getOtherLocationMale())
+                .sum(), 2L);
+        verify(formRepository, times(1)).findByCategory(FormType.Category.REGULAR);
+    }
+
+    @Test
+    void 어드민이_2차_전형자_중_특별_전형_성비를_조회한다() {
+        // given
+        List<Form> formList = List.of(
+                FormFixture.createForm(FormType.MEISTER_TALENT),
+                FormFixture.createForm(FormType.NATIONAL_BASIC_LIVING),
+                FormFixture.createMaleForm(FormType.MEISTER_TALENT),
+                FormFixture.createMaleForm(FormType.MULTICULTURAL),
+                FormFixture.createMaleForm(FormType.FARMING_AND_FISHING),
+                FormFixture.createMaleForm(FormType.ONE_PARENT),
+                FormFixture.createMaleForm(FormType.NEAR_POVERTY)
+        );
+        formList.forEach(Form::fail);
+
+        given(formRepository.findByCategory(FormType.Category.MEISTER_TALENT))
+                .willReturn(formList.stream()
+                        .filter(form -> form.getType().categoryEquals(FormType.Category.MEISTER_TALENT))
+                        .toList());
+        given(formRepository.findByCategory(FormType.Category.SOCIAL_INTEGRATION))
+                .willReturn(formList.stream()
+                        .filter(form -> form.getType().categoryEquals(FormType.Category.SOCIAL_INTEGRATION))
+                        .toList());
+        List<FormStatus> round = List.of(FormStatus.FAILED, FormStatus.PASSED);
+        GenderRatioRequest request = new GenderRatioRequest(round, FormType.Category.SPECIAL);
+
+        // when
+        List<GenderRatioResponse> responseList = queryGenderRatioUseCase.execute(request);
+
+        // then
+        assertEquals(responseList.stream()
+                .mapToLong(response -> response.getBusanMale() + response.getOtherLocationMale())
+                .sum(), 5L);
+        assertEquals(responseList.stream()
+                .mapToLong(response -> response.getBusanFemale() + response.getOtherLocationFemale())
+                .sum(), 2L);
+        verify(formRepository, times(1)).findByCategory(FormType.Category.MEISTER_TALENT);
+        verify(formRepository, times(1)).findByCategory(FormType.Category.SOCIAL_INTEGRATION);
+    }
+
+    @Test
+    void 어드민이_2차_전형자_중_정원_외_전형_성비를_조회한다() {
+        // given
+        List<Form> formList = List.of(
+                FormFixture.createForm(FormType.NATIONAL_VETERANS_EDUCATION),
+                FormFixture.createForm(FormType.SPECIAL_ADMISSION),
+                FormFixture.createMaleForm(FormType.NATIONAL_VETERANS_EDUCATION),
+                FormFixture.createMaleForm(FormType.SPECIAL_ADMISSION)
+        );
+        formList.forEach(Form::fail);
+
+        given(formRepository.findByCategory(FormType.Category.NATIONAL_VETERANS_EDUCATION))
+                .willReturn(formList.stream()
+                        .filter(form -> form.getType().categoryEquals(FormType.Category.NATIONAL_VETERANS_EDUCATION))
+                        .toList());
+        given(formRepository.findByCategory(FormType.Category.SPECIAL_ADMISSION))
+                .willReturn(formList.stream()
+                        .filter(form -> form.getType().categoryEquals(FormType.Category.SPECIAL_ADMISSION))
+                        .toList());
+        List<FormStatus> round = List.of(FormStatus.FAILED, FormStatus.PASSED);
+        GenderRatioRequest request = new GenderRatioRequest(round, FormType.Category.SUPERNUMERARY);
+
+        // when
+        List<GenderRatioResponse> responseList = queryGenderRatioUseCase.execute(request);
+
+        // then
+        assertEquals(responseList.stream()
+                .mapToLong(response -> response.getBusanMale() + response.getOtherLocationMale())
+                .sum(), 2L);
+        assertEquals(responseList.stream()
+                .mapToLong(response -> response.getBusanFemale() + response.getOtherLocationFemale())
+                .sum(), 2L);
+        verify(formRepository, times(1)).findByCategory(FormType.Category.NATIONAL_VETERANS_EDUCATION);
+        verify(formRepository, times(1)).findByCategory(FormType.Category.SPECIAL_ADMISSION);
+    }
+
+    @Test
+    void 어드민이_최종_합격자_중_일반_전형_성비를_조회한다() {
+        // given
+        List<Form> formList = List.of(
+                FormFixture.createForm(FormType.REGULAR),
+                FormFixture.createMaleForm(FormType.REGULAR),
+                FormFixture.createMaleForm(FormType.REGULAR)
+        );
+        formList.forEach(Form::pass);
+
+        given(formRepository.findByCategory(FormType.Category.REGULAR)).willReturn(formList);
+        List<FormStatus> round = List.of(FormStatus.PASSED);
+        GenderRatioRequest request = new GenderRatioRequest(round, FormType.Category.REGULAR);
+
+        // when
+        List<GenderRatioResponse> responseList = queryGenderRatioUseCase.execute(request);
+
+        // then
+        assertEquals(responseList.stream()
+                .mapToLong(response -> response.getBusanFemale() + response.getOtherLocationFemale())
+                .sum(), 1L);
+        assertEquals(responseList.stream()
+                .mapToLong(response -> response.getBusanMale() + response.getOtherLocationMale())
+                .sum(), 2L);
+        verify(formRepository, times(1)).findByCategory(FormType.Category.REGULAR);
+    }
+
+    @Test
+    void 어드민이_최종_합격자_중_특별_전형_성비를_조회한다() {
+        // given
+        List<Form> formList = List.of(
+                FormFixture.createForm(FormType.MEISTER_TALENT),
+                FormFixture.createForm(FormType.NATIONAL_BASIC_LIVING),
+                FormFixture.createMaleForm(FormType.MEISTER_TALENT),
+                FormFixture.createMaleForm(FormType.MULTICULTURAL),
+                FormFixture.createMaleForm(FormType.FARMING_AND_FISHING),
+                FormFixture.createMaleForm(FormType.ONE_PARENT),
+                FormFixture.createMaleForm(FormType.NEAR_POVERTY)
+        );
+        formList.forEach(Form::pass);
+
+        given(formRepository.findByCategory(FormType.Category.MEISTER_TALENT))
+                .willReturn(formList.stream()
+                        .filter(form -> form.getType().categoryEquals(FormType.Category.MEISTER_TALENT))
+                        .toList());
+        given(formRepository.findByCategory(FormType.Category.SOCIAL_INTEGRATION))
+                .willReturn(formList.stream()
+                        .filter(form -> form.getType().categoryEquals(FormType.Category.SOCIAL_INTEGRATION))
+                        .toList());
+        List<FormStatus> round = List.of(FormStatus.PASSED);
+        GenderRatioRequest request = new GenderRatioRequest(round, FormType.Category.SPECIAL);
+
+        // when
+        List<GenderRatioResponse> responseList = queryGenderRatioUseCase.execute(request);
+
+        // then
+        assertEquals(responseList.stream()
+                .mapToLong(response -> response.getBusanMale() + response.getOtherLocationMale())
+                .sum(), 5L);
+        assertEquals(responseList.stream()
+                .mapToLong(response -> response.getBusanFemale() + response.getOtherLocationFemale())
+                .sum(), 2L);
+        verify(formRepository, times(1)).findByCategory(FormType.Category.MEISTER_TALENT);
+        verify(formRepository, times(1)).findByCategory(FormType.Category.SOCIAL_INTEGRATION);
+    }
+
+    @Test
+    void 어드민이_최종_합격자_중_정원_외_전형_성비를_조회한다() {
+        // given
+        List<Form> formList = List.of(
+                FormFixture.createForm(FormType.NATIONAL_VETERANS_EDUCATION),
+                FormFixture.createForm(FormType.SPECIAL_ADMISSION),
+                FormFixture.createMaleForm(FormType.NATIONAL_VETERANS_EDUCATION),
+                FormFixture.createMaleForm(FormType.SPECIAL_ADMISSION)
+        );
+        formList.forEach(Form::firstPass);
+
+        given(formRepository.findByCategory(FormType.Category.NATIONAL_VETERANS_EDUCATION))
+                .willReturn(formList.stream()
+                        .filter(form -> form.getType().categoryEquals(FormType.Category.NATIONAL_VETERANS_EDUCATION))
+                        .toList());
+        given(formRepository.findByCategory(FormType.Category.SPECIAL_ADMISSION))
+                .willReturn(formList.stream()
+                        .filter(form -> form.getType().categoryEquals(FormType.Category.SPECIAL_ADMISSION))
+                        .toList());
+        List<FormStatus> round = List.of(FormStatus.FIRST_PASSED, FormStatus.FAILED, FormStatus.PASSED);
+        GenderRatioRequest request = new GenderRatioRequest(round, FormType.Category.SUPERNUMERARY);
+
+        // when
+        List<GenderRatioResponse> responseList = queryGenderRatioUseCase.execute(request);
+
+        // then
+        assertEquals(responseList.stream()
+                .mapToLong(response -> response.getBusanMale() + response.getOtherLocationMale())
+                .sum(), 2L);
+        assertEquals(responseList.stream()
+                .mapToLong(response -> response.getBusanFemale() + response.getOtherLocationFemale())
+                .sum(), 2L);
+        verify(formRepository, times(1)).findByCategory(FormType.Category.NATIONAL_VETERANS_EDUCATION);
+        verify(formRepository, times(1)).findByCategory(FormType.Category.SPECIAL_ADMISSION);
     }
 }
--- src/test/java/com/bamdoliro/maru/shared/fixture/FormFixture.java
+++ src/test/java/com/bamdoliro/maru/shared/fixture/FormFixture.java
@@ -118,6 +118,72 @@ public class FormFixture {
         return form;
     }
 
+    public static Form createMaleForm(FormType type) {
+        Form form = new Form(
+                new Applicant(
+                        "https://maru.com/photo.png",
+                        "김밤돌",
+                        new PhoneNumber("01012345678"),
+                        LocalDate.of(2005, 4, 15),
+                        Gender.MALE
+                ),
+                new Parent(
+                        "김이로",
+                        new PhoneNumber("01012345678"),
+                        "엄마",
+                        new Address(
+                                "18071",
+                                "부산광역시 가락대로1393",
+                                "부산소프트웨어마이스터고"
+                        )
+                ),
+                new Education(
+                        GraduationType.EXPECTED,
+                        "2021",
+                        new School("비전중학교", "경기도", "경기도 비전시 비전구 비전로 1", "7631003"),
+                        new Teacher("나교사", new PhoneNumber("0519701234"), new PhoneNumber("01012344321"))
+                ),
+                new Grade(
+                        new SubjectList(
+                                List.of(
+                                        new Subject(2, 1, "국어", AchievementLevel.A),
+                                        new Subject(2, 1, "수학", AchievementLevel.A),
+                                        new Subject(2, 1, "사회", AchievementLevel.A),
+                                        new Subject(2, 1, "과학", AchievementLevel.A),
+                                        new Subject(2, 1, "영어", AchievementLevel.A),
+                                        new Subject(2, 1, "체육", AchievementLevel.A),
+                                        new Subject(2, 2, "국어", AchievementLevel.A),
+                                        new Subject(2, 2, "수학", AchievementLevel.A),
+                                        new Subject(2, 2, "사회", AchievementLevel.A),
+                                        new Subject(2, 2, "과학", AchievementLevel.A),
+                                        new Subject(2, 2, "영어", AchievementLevel.A),
+                                        new Subject(2, 2, "체육", AchievementLevel.A),
+                                        new Subject(3, 1, "국어", AchievementLevel.A),
+                                        new Subject(3, 1, "수학", AchievementLevel.B),
+                                        new Subject(3, 1, "사회", AchievementLevel.A),
+                                        new Subject(3, 1, "과학", AchievementLevel.A),
+                                        new Subject(3, 1, "영어", AchievementLevel.A),
+                                        new Subject(3, 1, "체육", AchievementLevel.A)
+                                )),
+                        new Attendance(0, 0, 0, 2),
+                        new Attendance(2, 1, 0, 0),
+                        new Attendance(0, 0, 1, 0),
+                        8,
+                        2,
+                        1,
+                        new CertificateList(List.of(Certificate.COMPUTER_SPECIALIST_LEVEL_2))
+                ),
+                new Document(
+                        "하이난김밤돌",
+                        "공부열심히할게용"
+                ),
+                type,
+                UserFixture.createUser()
+        );
+        form.updateScore(new Score(1.0, 2, 3, 4));
+        return form;
+    }
+
     public static Form createRandomForm(User user) {
         return new Form(
                 new Applicant(
