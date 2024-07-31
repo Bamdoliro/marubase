e971ac1e42952fc5857dc6828bba710fc78b275a
cabbage16 <qocn16@gmail.com>
2024-07-31 20:51:50 +0900

    feat(#98): presigned-url 생성 기능 - presigned-url을 생성하는 기능을 만들었어요.

---
+++ src/main/java/com/bamdoliro/maru/infrastructure/s3/UploadFileService.java
@@ -0,0 +1,95 @@
+package com.bamdoliro.maru.infrastructure.s3;
+
+import com.amazonaws.HttpMethod;
+import com.amazonaws.SdkClientException;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.Headers;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.bamdoliro.maru.infrastructure.s3.dto.response.UploadResponse;
+import com.bamdoliro.maru.infrastructure.s3.exception.FailedToSaveException;
+import com.bamdoliro.maru.infrastructure.s3.validator.FileValidator;
+import com.bamdoliro.maru.shared.config.properties.S3Properties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Date;
+
+@Service
+@RequiredArgsConstructor
+public class UploadFileService {
+
+    private final S3Properties s3Properties;
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${spring.cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public UploadResponse execute(MultipartFile file, String folder, String fileName, FileValidator validator) {
+        validator.validate(file);
+        String fullFileName = createFileName(folder, fileName);
+
+        try {
+            PutObjectRequest request = new PutObjectRequest(
+                    s3Properties.getBucket(),
+                    fullFileName,
+                    file.getInputStream(),
+                    getObjectMetadata(file)
+            ).withCannedAcl(CannedAccessControlList.PublicRead);
+
+            amazonS3Client.putObject(request);
+        } catch (SdkClientException | IOException e) {
+            throw new FailedToSaveException();
+        }
+
+        return new UploadResponse(
+                amazonS3Client.getUrl(s3Properties.getBucket(), fullFileName).toString()
+        );
+    }
+
+    public UploadResponse getPresignedUrl(String folder, String fileName) {
+        String fullFileName = createFileName(folder, fileName);
+        GeneratePresignedUrlRequest request = getGeneratePresignedUrlRequest(bucket, fileName);
+
+        return new UploadResponse(
+                amazonS3Client.generatePresignedUrl(request).toString()
+        );
+    }
+
+    private GeneratePresignedUrlRequest getGeneratePresignedUrlRequest(String bucket, String fileName) {
+        GeneratePresignedUrlRequest request = new GeneratePresignedUrlRequest(bucket, fileName)
+                .withMethod(HttpMethod.PUT)
+                .withExpiration(getPresignedUrlExpiration());
+        request.addRequestParameter(
+                Headers.S3_CANNED_ACL,
+                CannedAccessControlList.PublicRead.toString()
+        );
+
+        return request;
+    }
+
+    private Date getPresignedUrlExpiration() {
+        Date expiration = new Date();
+        long expTimeMillis = expiration.getTime();
+        expTimeMillis += 1000 * 60 * 60;
+        expiration.setTime(expTimeMillis);
+
+        return expiration;
+    }
+
+    private String createFileName(String folder, String fileName) {
+        return folder + "/" + fileName;
+    }
+
+    private ObjectMetadata getObjectMetadata(MultipartFile file) {
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentLength(file.getSize());
+        objectMetadata.setContentType(file.getContentType());
+        return objectMetadata;
+    }
+}
