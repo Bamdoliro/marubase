8601eae8d1726b76a6cc922c46ee68b5b7267c7d
cabbage16 <qocn16@gmail.com>
2024-08-16 04:40:57 +0900

    perf(#111): 공지사항 조회 시 파일 이름 추가 - 공지사항 조회 시 파일 이름을 볼 수 있도록 추가했어요.

--- src/main/java/com/bamdoliro/maru/presentation/notice/dto/response/NoticeResponse.java
+++ src/main/java/com/bamdoliro/maru/presentation/notice/dto/response/NoticeResponse.java
@@ -10,6 +10,7 @@ public class NoticeResponse {
 
     private final String title;
     private final String content;
+    private final String fileName;
     private final String fileUrl;
     private final LocalDateTime createdAt;
     private final LocalDateTime updatedAt;
@@ -17,6 +18,7 @@ public class NoticeResponse {
     public NoticeResponse(Notice notice, String fileUrl) {
         this.title = notice.getTitle();
         this.content = notice.getContent();
+        this.fileName = notice.getFileName();
         this.fileUrl = fileUrl;
         this.createdAt = notice.getCreatedAt();
         this.updatedAt = notice.getUpdatedAt();
