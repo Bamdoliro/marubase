4bd2de348a1e122cddbd8a444c5c6a2c2ddbf330
jyj1289 <jin37091289@gmail.com>
2024-09-08 23:59:23 +0900

    feat(#119): 특례입학 템플릿 추가 - 특례입학 템플릿을 추가했어요.

--- src/main/java/com/bamdoliro/maru/infrastructure/thymeleaf/Templates.java
+++ src/main/java/com/bamdoliro/maru/infrastructure/thymeleaf/Templates.java
@@ -13,4 +13,6 @@ public class Templates {
     public static final String PROOF_OF_APPLICATION = "proof-of-application";
     public static final String GRADE_TABLE = "grade-table";
     public static final String WRITTEN_OATH = "written-oath";
+    public static final String SPECIAL_ADMISSION = "special-admission";
+    public static final String CONFIRMATION = "confirmation";
 }
