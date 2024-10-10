3ce2d6fdd9f4711e7416810bc6eca73331ec14d7
cabbage16 <qocn16@gmail.com>
2024-10-10 13:57:39 +0900

    perf(#145): JWT 서명 알고리즘 명시적 선언 - JWT로 인증 시 서명 알고리즘이 HS256이 아닌 경우, 잘못된 토큰 에러가 아닌 잘못된 서명 알고리즘 에러를 발생하도록 했어요.

--- src/main/java/com/bamdoliro/maru/domain/auth/exception/error/AuthErrorProperty.java
+++ src/main/java/com/bamdoliro/maru/domain/auth/exception/error/AuthErrorProperty.java
@@ -12,6 +12,7 @@ public enum AuthErrorProperty implements ErrorProperty {
     EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
     EMPTY_TOKEN(HttpStatus.UNAUTHORIZED, "인증이 필요합니다."),
+    INVALID_SIGNATURE_ALGORITHM(HttpStatus.UNAUTHORIZED, "유효하지 않은 서명 알고리즘입니다."),
     AUTHORITY_MISMATCH(HttpStatus.UNAUTHORIZED, "권한이 없습니다."),
     WRONG_LOGIN(HttpStatus.UNAUTHORIZED, "사용자가 없거나 비밀번호가 틀렸습니다."),
     ;
--- src/main/java/com/bamdoliro/maru/domain/auth/service/TokenService.java
+++ src/main/java/com/bamdoliro/maru/domain/auth/service/TokenService.java
@@ -3,11 +3,15 @@ package com.bamdoliro.maru.domain.auth.service;
 import com.bamdoliro.maru.domain.auth.domain.Token;
 import com.bamdoliro.maru.domain.auth.domain.type.TokenType;
 import com.bamdoliro.maru.domain.auth.exception.ExpiredTokenException;
+import com.bamdoliro.maru.domain.auth.exception.InvalidSignatureAlgorithmException;
 import com.bamdoliro.maru.domain.auth.exception.InvalidTokenException;
 import com.bamdoliro.maru.domain.user.domain.User;
 import com.bamdoliro.maru.domain.user.service.UserFacade;
 import com.bamdoliro.maru.infrastructure.persistence.auth.TokenRepository;
 import com.bamdoliro.maru.shared.config.properties.JwtProperties;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
@@ -18,7 +22,9 @@ import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.util.Base64;
 import java.util.Date;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
@@ -71,6 +77,8 @@ public class TokenService {
 
     private Claims extractAllClaims(String token) {
         try {
+            validateSignatureAlgorithm(token);
+
             return Jwts.parserBuilder()
                     .setSigningKey(getSigningKey(jwtProperties.getSecretKey()))
                     .build()
@@ -78,8 +86,23 @@ public class TokenService {
                     .getBody();
         } catch (ExpiredJwtException e) {
             throw new ExpiredTokenException();
+        } catch (InvalidSignatureAlgorithmException e) {
+            throw e;
         } catch (Exception e) {
             throw new InvalidTokenException();
+        }
+    }
+
+    private void validateSignatureAlgorithm(String token) throws JsonProcessingException {
+        String[] tokenParts = token.split("\\.");
+        String headerJson = new String(Base64.getDecoder().decode(tokenParts[0]));
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> headerMap = objectMapper.readValue(headerJson, new TypeReference<>() {});
+        String algorithm = (String) headerMap.get("alg");
+
+        if (!algorithm.equals(SignatureAlgorithm.HS256.getValue())) {
+            throw new InvalidSignatureAlgorithmException();
         }
     }
 
