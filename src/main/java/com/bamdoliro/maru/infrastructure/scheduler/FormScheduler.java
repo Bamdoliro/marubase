40b24623fbb5a3efd71896936c0b7df3638d5c9b
leegunwoooo <leegunwoo0325@gmail.com>
2025-05-09 11:52:46 +0900

    feat(#209) : 입력받는 시간을 전달하는 usecase 작성

--- src/main/java/com/bamdoliro/maru/infrastructure/scheduler/FormScheduler.java
+++ src/main/java/com/bamdoliro/maru/infrastructure/scheduler/FormScheduler.java
@@ -2,17 +2,34 @@ package com.bamdoliro.maru.infrastructure.scheduler;
 
 import com.bamdoliro.maru.application.form.SelectFirstPassUseCase;
 import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.stereotype.Component;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.concurrent.ScheduledFuture;
 
 @RequiredArgsConstructor
 @Component
 public class FormScheduler {
 
     private final SelectFirstPassUseCase selectFirstPassUseCase;
+    private final TaskScheduler taskScheduler;
 
-    @Scheduled(cron = "${schedule.select-first-pass-cron}")
-    public void selectFirstPass() {
-        selectFirstPassUseCase.execute();
+    private ScheduledFuture<?> scheduledTask;
+
+    public void selectFirstPass(LocalDateTime scheduledTime) {
+        if (scheduledTime.isBefore(LocalDateTime.now())) {
+            throw new IllegalArgumentException("이미 지난 시간입니다");
+        }
+
+        if (scheduledTask != null && !scheduledTask.isCancelled()) {
+            scheduledTask.cancel(false);
+        }
+
+        scheduledTask = taskScheduler.schedule(
+                selectFirstPassUseCase::execute,
+                Timestamp.valueOf(scheduledTime)
+        );
     }
 }
