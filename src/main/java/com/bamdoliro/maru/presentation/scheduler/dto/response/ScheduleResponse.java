3aabab2197eef8ecf48eed3e712b69322190e857
leegunwoooo <leegunwoo0325@gmail.com>
2025-05-09 11:59:53 +0900

    refactor(#209) : json응답의 형식을 지정

--- src/main/java/com/bamdoliro/maru/presentation/scheduler/dto/response/ScheduleResponse.java
+++ src/main/java/com/bamdoliro/maru/presentation/scheduler/dto/response/ScheduleResponse.java
@@ -1,14 +1,17 @@
 package com.bamdoliro.maru.presentation.scheduler.dto.response;
 
 import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 
 @Getter
 public class ScheduleResponse {
 
-    private LocalDateTime schedule_select_first_pass;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime scheduleSelectFirstPass;
 
-    public ScheduleResponse(LocalDateTime schedule_select_first_pass) {
-        this.schedule_select_first_pass = schedule_select_first_pass;
+    public ScheduleResponse(LocalDateTime scheduleSelectFirstPass) {
+        this.scheduleSelectFirstPass = scheduleSelectFirstPass;
     }
 }
