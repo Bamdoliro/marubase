c9d6267945fcdd8c3589e8e60253ab0bc3286178
leegunwoooo <leegunwoo0325@gmail.com>
2025-05-09 11:59:21 +0900

    refactor(#209) : request dto에 파라미터 없는 기본생성자 추가

--- src/main/java/com/bamdoliro/maru/presentation/scheduler/dto/request/ScheduleRequest.java
+++ src/main/java/com/bamdoliro/maru/presentation/scheduler/dto/request/ScheduleRequest.java
@@ -1,15 +1,18 @@
 package com.bamdoliro.maru.presentation.scheduler.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-
+import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ScheduleRequest {
 
-    @NotBlank(message = "필수값입니다.")
-    private LocalDateTime schedule_select_first_pass;
+    @NotNull(message = "필수값입니다.")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime scheduleSelectFirstPass;
 }
