1dbae244d5eff17310ee58a6966b605ed93ace16
leegunwoooo <leegunwoo0325@gmail.com>
2025-07-14 11:00:07 +0900

    refactor(#209) : 컨트롤러의 엔드포인트 변경
    엔드포인트에 여러단어가 들어가기에 엔드포인트를 변경하였습니다.

--- src/main/java/com/bamdoliro/maru/presentation/scheduler/SchedulerController.java
+++ src/main/java/com/bamdoliro/maru/presentation/scheduler/SchedulerController.java
@@ -12,16 +12,18 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/schedule")
 @RequiredArgsConstructor
 public class SchedulerController {
 
     private final ScheduleFirstPassUseCase scheduleFirstPassUseCase;
 
 
-    @PostMapping("/updateSchedule")
+    @PostMapping("/first-pass")
     public SingleCommonResponse<ScheduleResponse> updateSchedule(
             @AuthenticationPrincipal(authority = Authority.ADMIN) User user,
             @RequestBody @Valid ScheduleRequest scheduleRequest) {
