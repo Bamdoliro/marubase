cc380c145eae460cf54da6ecb63aaa21841d50ea
leegunwoo <leegunwoo0325@gmail.com>
2025-08-20 08:30:26 +0900

    test(#209) : 테스트 코드 수정
    테스트 코드를 바뀐 코드에 맞게 수정하였습니다.

--- src/test/java/com/bamdoliro/maru/presentation/scheduler/SchedulerControllerTest.java
+++ src/test/java/com/bamdoliro/maru/presentation/scheduler/SchedulerControllerTest.java
@@ -2,6 +2,7 @@ package com.bamdoliro.maru.presentation.scheduler;
 
 import com.bamdoliro.maru.domain.user.domain.User;
 import com.bamdoliro.maru.presentation.scheduler.dto.request.ScheduleRequest;
+import com.bamdoliro.maru.presentation.scheduler.dto.response.ScheduleResponse;
 import com.bamdoliro.maru.shared.fixture.AuthFixture;
 import com.bamdoliro.maru.shared.fixture.UserFixture;
 import com.bamdoliro.maru.shared.util.RestDocsTestSupport;
@@ -27,11 +28,12 @@ class SchedulerControllerTest extends RestDocsTestSupport {
     @Test
     void 일차_합격_기간을_설정한다() throws Exception {
         User user = UserFixture.createAdminUser();
-        ScheduleRequest request = new ScheduleRequest(LocalDateTime.parse("2025-05-22T10:00:00"));
+        ScheduleRequest request = new ScheduleRequest(LocalDateTime.parse("2025-10-27T15:00:00"));
 
         given(authenticationArgumentResolver.supportsParameter(any())).willReturn(true);
         given(authenticationArgumentResolver.resolveArgument(any(), any(), any(), any())).willReturn(user);
-        given(scheduleFirstPassUseCase.execute(any(LocalDateTime.class))).willReturn(request.getScheduleSelectFirstPass());
+        given(scheduleFirstPassUseCase.execute(any(ScheduleRequest.class)))
+                .willReturn(new ScheduleResponse(request.getScheduleSelectFirstPass()));
 
         mockMvc.perform(post("/schedule/first-pass")
                         .header(HttpHeaders.AUTHORIZATION, AuthFixture.createAuthHeader())
@@ -39,9 +41,7 @@ class SchedulerControllerTest extends RestDocsTestSupport {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(toJson(request))
                 )
-
                 .andExpect(status().isOk())
-
                 .andDo(restDocs.document(
                         requestHeaders(
                                 headerWithName(HttpHeaders.AUTHORIZATION)
@@ -50,12 +50,12 @@ class SchedulerControllerTest extends RestDocsTestSupport {
                         requestFields(
                                 fieldWithPath("scheduleSelectFirstPass")
                                         .type(JsonFieldType.STRING)
-                                        .description("입력된 기간")
+                                        .description("설정할 일차 합격자 선발 스케줄")
                         ),
                         responseFields(
                                 fieldWithPath("code").description("응답 코드"),
                                 fieldWithPath("message").description("응답 메시지"),
-                                fieldWithPath("data.scheduleSelectFirstPass").description("일차 합격자 선발 스케줄 설정 여부")
+                                fieldWithPath("data.scheduleSelectFirstPass").description("설정된 일차 합격자 선발 스케줄 시간")
                         )
                 ));
     }
