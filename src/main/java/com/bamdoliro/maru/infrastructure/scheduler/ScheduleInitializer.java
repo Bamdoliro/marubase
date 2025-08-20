aa6d8270e538b8fdb49ecab82ef0f4266806b6b2
leegunwoo <leegunwoo0325@gmail.com>
2025-08-20 10:09:28 +0900

    refactor(#209) : 시간 저장
    서버가 시작될 때 db에 시간이 저장되도록 하였습니다.

--- src/main/java/com/bamdoliro/maru/infrastructure/scheduler/ScheduleInitializer.java
+++ src/main/java/com/bamdoliro/maru/infrastructure/scheduler/ScheduleInitializer.java
@@ -1,17 +1,26 @@
 package com.bamdoliro.maru.infrastructure.scheduler;
 
+import com.bamdoliro.maru.domain.scheduler.FirstPassSchedule;
+import com.bamdoliro.maru.infrastructure.persistence.scheduler.FirstPassScheduleRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 @Component
 @RequiredArgsConstructor
 public class ScheduleInitializer {
 
     private final FormScheduler formScheduler;
+    private final FirstPassScheduleRepository firstPassScheduleRepository;
 
     @PostConstruct
     public void init() {
+        firstPassScheduleRepository.findById(1L)
+                .orElseGet(() -> firstPassScheduleRepository.save(
+                        new FirstPassSchedule(LocalDateTime.parse("2025-10-25T15:00:00"))
+                ));
         formScheduler.restoreSchedule();
     }
 }
