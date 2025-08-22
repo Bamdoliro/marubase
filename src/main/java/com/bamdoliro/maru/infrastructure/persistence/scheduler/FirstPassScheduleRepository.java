c358d442cd371e4b8207cf86f9d0bc786520dbc8
leegunwoo <leegunwoo0325@gmail.com>
2025-08-22 19:21:57 +0900

    refactor(#209) : Repository 파일 구조 변경
    - FirstPassSchedule가 domain 디렉토리 안으로 들어가 import 문이 변경되었습니다.

--- src/main/java/com/bamdoliro/maru/infrastructure/persistence/scheduler/FirstPassScheduleRepository.java
+++ src/main/java/com/bamdoliro/maru/infrastructure/persistence/scheduler/FirstPassScheduleRepository.java
@@ -1,6 +1,6 @@
 package com.bamdoliro.maru.infrastructure.persistence.scheduler;
 
-import com.bamdoliro.maru.domain.scheduler.FirstPassSchedule;
+import com.bamdoliro.maru.domain.scheduler.domain.FirstPassSchedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FirstPassScheduleRepository extends JpaRepository<FirstPassSchedule, Long> {
