41b329b5e356079f46395416b7f043fd91ffa93a
leegunwoo <leegunwoo0325@gmail.com>
2025-08-22 19:21:13 +0900

    refactor(#209) : 파일 구조 변경
    - FirstPassScheduleProperty 가 error 디렉토리 안으로 들어가면서 import 문을 변경하였습니다

--- src/main/java/com/bamdoliro/maru/domain/scheduler/exception/CannotSchedulePastTimeException.java
+++ src/main/java/com/bamdoliro/maru/domain/scheduler/exception/CannotSchedulePastTimeException.java
@@ -1,5 +1,6 @@
 package com.bamdoliro.maru.domain.scheduler.exception;
 
+import com.bamdoliro.maru.domain.scheduler.exception.error.FirstPassScheduleProperty;
 import com.bamdoliro.maru.shared.error.MaruException;
 
 public class CannotSchedulePastTimeException extends MaruException {
