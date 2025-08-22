3d1c350db06a0416dd5167d4f4b9cecfa01749a6
leegunwoo <leegunwoo0325@gmail.com>
2025-08-22 19:56:15 +0900

    feat(#209) : 가장 최근 일정 찾는 메서드 추가
    가장 최근에 생성된 일정을 조회하는 JPA 메서드가 필요해 작성하였습니다.

--- src/main/java/com/bamdoliro/maru/infrastructure/persistence/scheduler/FirstPassScheduleRepository.java
+++ src/main/java/com/bamdoliro/maru/infrastructure/persistence/scheduler/FirstPassScheduleRepository.java
@@ -3,5 +3,10 @@ package com.bamdoliro.maru.infrastructure.persistence.scheduler;
 import com.bamdoliro.maru.domain.scheduler.domain.FirstPassSchedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FirstPassScheduleRepository extends JpaRepository<FirstPassSchedule, Long> {
+
+    Optional<FirstPassSchedule> findTopByOrderByCreatedAtDesc();
+
 }
