fde14bf068826d77affb52e59c123770373187df
leegunwoo <leegunwoo0325@gmail.com>
2025-09-04 01:06:22 +0900

    test(#234) : 테스트 코드 시간 비교 수정
    - 제가 짠 코드 대로 하면 밀리초까지 비교하기에 초 단위까지만 비교하도록 변경하였습니다.

--- src/test/java/com/bamdoliro/maru/application/fair/CreateAdmissionFairUseCaseTest.java
+++ src/test/java/com/bamdoliro/maru/application/fair/CreateAdmissionFairUseCaseTest.java
@@ -11,6 +11,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.temporal.ChronoUnit;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
@@ -40,7 +42,7 @@ class CreateAdmissionFairUseCaseTest {
         // then
         verify(fairRepository, times(1)).save(captor.capture());
         Fair savedFair = captor.getValue();
-        assertEquals(fair.getStart(), savedFair.getStart());
+        assertEquals(fair.getStart().truncatedTo(ChronoUnit.SECONDS), savedFair.getStart().truncatedTo(ChronoUnit.SECONDS));
         assertEquals(fair.getPlace(), savedFair.getPlace());
        }
 }
