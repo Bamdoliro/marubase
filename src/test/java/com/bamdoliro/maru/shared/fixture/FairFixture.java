8e2215008e33f25df8c0e1c23aa62e09737c5611
leegunwoo <leegunwoo0325@gmail.com>
2025-09-04 01:05:10 +0900

    test(#234) : 입학설명회 Request 테스트 생성 코드 수정
    - 입학설명회 시간과 Request 의 시간을 맞추었습니다.

--- src/test/java/com/bamdoliro/maru/shared/fixture/FairFixture.java
+++ src/test/java/com/bamdoliro/maru/shared/fixture/FairFixture.java
@@ -27,7 +27,7 @@ public class FairFixture {
     }
     public static CreateFairRequest createFairRequest() {
         return new CreateFairRequest(
-                LocalDateTime.of(2025, 8, 26, 11, 0),
+                LocalDateTime.now().plusDays(1),
                 120,
                 "부산소프트웨어마이스터고등학교 1층 SRC관",
                 FairType.STUDENT_AND_PARENT,
