0648deedbedf6ee5706cc282b914c121f55e2e98
leegunwoo <leegunwoo0325@gmail.com>
2025-09-04 00:37:08 +0900

    test(#234) : 입학설명회 테스트 생성 코드 수정
    - 과거로 되어 있던 시간이 현재로 바뀌도록 수정

--- src/test/java/com/bamdoliro/maru/shared/fixture/FairFixture.java
+++ src/test/java/com/bamdoliro/maru/shared/fixture/FairFixture.java
@@ -17,7 +17,7 @@ public class FairFixture {
 
     public static Fair createFair() {
         return new Fair(
-                LocalDateTime.of(2025, 8, 26, 11, 0),
+                LocalDateTime.now(),
                 120,
                 "부산소프트웨어마이스터고등학교 1층 SRC관",
                 FairType.STUDENT_AND_PARENT,
