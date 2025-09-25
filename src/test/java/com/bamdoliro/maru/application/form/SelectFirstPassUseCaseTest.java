daa1b0d9c8f53e0abc16055a674bc1b1b45434f8
hsm <hll2071@naver.com>
2025-09-26 00:17:32 +0900

    test(#249) : 바뀐 코드에 맞게 테스트 수정
    테스트에서도 승인된 원서로 로직이 돌아가게 수정했습니다.

--- src/test/java/com/bamdoliro/maru/application/form/SelectFirstPassUseCaseTest.java
+++ src/test/java/com/bamdoliro/maru/application/form/SelectFirstPassUseCaseTest.java
@@ -54,7 +54,7 @@ class SelectFirstPassUseCaseTest {
         formList.addAll(FormFixture.generateOtherRegionFormList(userList.subList(userList.size() / 2, userList.size())));
         formList.forEach(form -> {
             assignExaminationNumberService.execute(form);
-            form.receive();
+            form.approve();
             calculateFormScoreService.execute(form);
             formRepository.save(form);
         });
