e30f3557d042d828d18a9bd2505e843b70efd90a
이건우 <162273778+leegunwoooo@users.noreply.github.com>
2025-10-15 22:30:05 +0900

    Revert "[버그] 2차 점수 엑셀 입력시 NPE 발생"

--- src/main/java/com/bamdoliro/maru/application/form/UpdateSecondRoundScoreUseCase.java
+++ src/main/java/com/bamdoliro/maru/application/form/UpdateSecondRoundScoreUseCase.java
@@ -110,22 +110,20 @@ public class UpdateSecondRoundScoreUseCase {
         Cell isShowCell = row.getCell(6);
         boolean isShow = false;
 
-        if (examinationNumberCell == null || examinationNumberCell.getCellType() != CellType.NUMERIC) {
-            if (examinationNumberCell != null) {
-                setErrorCell(examinationNumberCell, "타입 불일치");
-            }
+        if (examinationNumberCell.getCellType() != CellType.NUMERIC) {
+            setErrorCell(examinationNumberCell, "타입 불일치");
             cellList.add(examinationNumberCell);
         }
-        if (nameCell == null || nameCell.getCellType() != CellType.STRING) {
-            if (nameCell != null) setErrorCell(nameCell, "타입 불일치");
+        if (nameCell.getCellType() != CellType.STRING) {
+            setErrorCell(nameCell, "타입 불일치");
             cellList.add(nameCell);
         }
-        if (typeCell == null || typeCell.getCellType() != CellType.STRING) {
-            if (typeCell != null) setErrorCell(typeCell, "타입 불일치");
+        if (typeCell.getCellType() != CellType.STRING) {
+            setErrorCell(typeCell, "타입 불일치");
             cellList.add(typeCell);
         }
-        if (isShowCell == null || isShowCell.getCellType() != CellType.FORMULA) {
-            if (isShowCell != null) setErrorCell(isShowCell, "타입 불일치");
+        if (isShowCell.getCellType() != CellType.FORMULA) {
+            setErrorCell(isShowCell, "타입 불일치");
             cellList.add(isShowCell);
         } else {
             isShow = isShowCell.getBooleanCellValue();
