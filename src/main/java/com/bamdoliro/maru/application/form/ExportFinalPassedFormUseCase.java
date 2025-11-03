53d9a2d00e78e0b614afb26edbf1d0ba4b55c368
leegunwoo <leegunwoo0325@gmail.com>
2025-11-03 23:22:38 +0900

    refactor(#309) : 출력되는 원서들의 status를 입학에서 최종합격으로 변경

--- src/main/java/com/bamdoliro/maru/application/form/ExportFinalPassedFormUseCase.java
+++ src/main/java/com/bamdoliro/maru/application/form/ExportFinalPassedFormUseCase.java
@@ -22,7 +22,7 @@ public class ExportFinalPassedFormUseCase {
     private final XlsxGenerator xlsxGenerator;
 
     public Resource execute() throws IOException {
-        List<Form> formList = formFacade.getSortedFormList(FormStatus.ENTERED);
+        List<Form> formList = formFacade.getSortedFormList(FormStatus.PASSED);
 
         List<Function<Form, Object>> columnList = List.of(
                 Form::getId,
