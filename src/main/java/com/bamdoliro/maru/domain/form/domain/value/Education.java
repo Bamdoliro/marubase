7ed1e81194ac23015205a1bcb1a14cdcc95d028e
이건우 <162273778+leegunwoooo@users.noreply.github.com>
2025-11-11 08:19:43 +0900

    Revert "[새기능] 원서 입력 시 학번도 받도록 수정"

--- src/main/java/com/bamdoliro/maru/domain/form/domain/value/Education.java
+++ src/main/java/com/bamdoliro/maru/domain/form/domain/value/Education.java
@@ -17,9 +17,6 @@ import lombok.NoArgsConstructor;
 @Embeddable
 public class Education {
 
-    @Column(nullable = false, length = 10)
-    private String studentNumber;
-
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 25)
     private GraduationType graduationType;
