4b2a6963f152d00169eb8f3490827aee581f1110
leegunwoo <leegunwoo0325@gmail.com>
2025-11-10 21:13:35 +0900

    refactor(#315) : 모든 Double 자료형을 소숫점 3자리까지 반올림

--- src/main/java/com/bamdoliro/maru/presentation/analysis/dto/response/GradeDistributionResponse.java
+++ src/main/java/com/bamdoliro/maru/presentation/analysis/dto/response/GradeDistributionResponse.java
@@ -2,6 +2,7 @@ package com.bamdoliro.maru.presentation.analysis.dto.response;
 
 import com.bamdoliro.maru.domain.form.domain.type.FormType;
 import com.bamdoliro.maru.infrastructure.persistence.form.vo.GradeVo;
+import com.bamdoliro.maru.shared.util.MathUtil;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,11 +21,11 @@ public class GradeDistributionResponse {
 
     public GradeDistributionResponse(GradeVo vo) {
         this.type = vo.getType();
-        this.firstRoundMax = vo.getFirstRoundMax();
-        this.firstRoundMin = vo.getFirstRoundMin();
-        this.firstRoundAvg = vo.getFirstRoundAvg();
-        this.totalMax = vo.getTotalMax();
-        this.totalMin = vo.getTotalMin();
-        this.totalAvg = vo.getTotalAvg();
+        this.firstRoundMax = MathUtil.roundTo(vo.getFirstRoundMax(), 3);
+        this.firstRoundMin = MathUtil.roundTo(vo.getFirstRoundMin(), 3);
+        this.firstRoundAvg = MathUtil.roundTo(vo.getFirstRoundAvg(), 3);
+        this.totalMax = MathUtil.roundTo(vo.getTotalMax(), 3);
+        this.totalMin = MathUtil.roundTo(vo.getTotalMin(), 3);
+        this.totalAvg = MathUtil.roundTo(vo.getTotalAvg(), 3);
     }
 }
