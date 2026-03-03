f1334888dee22101653a8eba2cb31a3711f54ca1
leegunwoo <leegunwoo0325@gmail.com>
2026-03-03 17:40:05 +0900

    refactor(#344) : css 경로 문법 수정

--- src/main/java/com/bamdoliro/maru/shared/config/WebConfig.java
+++ src/main/java/com/bamdoliro/maru/shared/config/WebConfig.java
@@ -25,8 +25,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "https://maru.bamdoliro.com",
-                        "https://madmin.bamdoliro.com"
+                        "*"
                 )
                 .allowedMethods(
                         HttpMethod.GET.name(),
