916874beb9e975fed121b2f60340b1bbcdc8e6ec
leegunwoo <leegunwoo0325@gmail.com>
2026-03-20 10:31:46 +0900

    refactor(#352) : 구현체인 S3Client가 아닌 인터페이스인 AmazonS3에 의존도록 코드를 수정하였어요

--- src/main/java/com/bamdoliro/maru/shared/config/S3Config.java
+++ src/main/java/com/bamdoliro/maru/shared/config/S3Config.java
@@ -3,7 +3,7 @@ package com.bamdoliro.maru.shared.config;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.bamdoliro.maru.shared.config.properties.S3Properties;
 import lombok.RequiredArgsConstructor;
@@ -24,7 +24,7 @@ public class S3Config {
     private String endpoint;
 
     @Bean
-    public AmazonS3Client amazonS3Client() {
+    public AmazonS3 amazonS3() {
         BasicAWSCredentials awsCredentials = new BasicAWSCredentials(
                 s3Properties.getAccessKey(), s3Properties.getSecretKey());
 
@@ -40,6 +40,6 @@ public class S3Config {
             builder.withRegion(region);
         }
 
-        return (AmazonS3Client) builder.build();
+        return builder.build();
     }
 }
