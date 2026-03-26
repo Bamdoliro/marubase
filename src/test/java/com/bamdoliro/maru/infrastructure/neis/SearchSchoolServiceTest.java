d009b7aeaa03182bb3c293cf96cdb3e2e487e7e9
leegunwoo <leegunwoo0325@gmail.com>
2026-03-26 17:20:15 +0900

    test(#357) : 테스트 Disabled - 테스트에서 실제 api를 호출하여 Disabled하였어요. - 추후 단위테스트로 리팩토링 할 것입니다.

--- src/test/java/com/bamdoliro/maru/infrastructure/neis/SearchSchoolServiceTest.java
+++ src/test/java/com/bamdoliro/maru/infrastructure/neis/SearchSchoolServiceTest.java
@@ -2,6 +2,7 @@ package com.bamdoliro.maru.infrastructure.neis;
 
 import com.bamdoliro.maru.presentation.school.dto.response.SchoolResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@Disabled("테스트 실행 시 실제 조회가 발생하므로 비활성화")
 @ActiveProfiles("test")
 @SpringBootTest
 class SearchSchoolServiceTest {
@@ -23,7 +25,7 @@ class SearchSchoolServiceTest {
         String q = "비전";
         List<SchoolResponse> responseList = searchSchoolService.execute(q);
         assertEquals(1, responseList.size());
-        assertEquals("비전중학교", responseList.get(0).getName());
+        assertEquals("비전중학교", responseList.getFirst().getName());
     }
 
     @Test
