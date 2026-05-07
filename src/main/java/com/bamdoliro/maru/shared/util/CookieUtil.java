9053ab1f9cc8d192fb220102cb64138cfc982390
leegunwoo <leegunwoo0325@gmail.com>
2026-05-07 14:54:59 +0900

    Revert "refactor : 설정 변경"
    This reverts commit 803c7ca25c148dd2ed93d423257f67e3bba633f0.

--- src/main/java/com/bamdoliro/maru/shared/util/CookieUtil.java
+++ src/main/java/com/bamdoliro/maru/shared/util/CookieUtil.java
@@ -39,7 +39,7 @@ public class CookieUtil {
         return ResponseCookie.from(name, value)
                 .httpOnly(true)
                 .secure(true)
-                .sameSite("None")
+                .sameSite("Strict")
                 .domain(DOMAIN)
                 .maxAge(maxAge)
                 .build();
