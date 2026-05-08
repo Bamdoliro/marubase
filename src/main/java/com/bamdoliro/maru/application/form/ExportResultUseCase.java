15e9c68d877f308fb29c914b028d412f91c498f7
leegunwoo <leegunwoo0325@gmail.com>
2026-05-08 11:13:20 +0900

    refactor(#393) : 메서드 시그니처 변경점 적용

--- src/main/java/com/bamdoliro/maru/application/form/ExportResultUseCase.java
+++ src/main/java/com/bamdoliro/maru/application/form/ExportResultUseCase.java
@@ -2,6 +2,7 @@ package com.bamdoliro.maru.application.form;
 
 import com.bamdoliro.maru.domain.form.domain.Form;
 import com.bamdoliro.maru.domain.form.service.FormFacade;
+import com.bamdoliro.maru.infrastructure.xlsx.FormColumn;
 import com.bamdoliro.maru.infrastructure.xlsx.XlsxGenerator;
 import com.bamdoliro.maru.shared.annotation.UseCase;
 import com.bamdoliro.maru.shared.util.MathUtil;
@@ -11,7 +12,6 @@ import org.springframework.core.io.Resource;
 import java.io.IOException;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.function.Function;
 
 @RequiredArgsConstructor
 @UseCase
@@ -23,65 +23,35 @@ public class ExportResultUseCase {
     public Resource execute() throws IOException {
         List<Form> formList = formFacade.getSortedFormList(null);
 
-        List<Function<Form, Object>> columnList = List.of(
-                Form::getId,
-                Form::getExaminationNumber,
-                form -> form.getOriginalType().getDescription(),
-                form -> form.getType().getDescription(),
-                form -> form.getStatus().getDescription(),
-                form -> form.getApplicant().getName(),
-                form -> form.getApplicant().getGender().getDescription(),
-                form -> form.getApplicant().getBirthday().format(DateTimeFormatter.BASIC_ISO_DATE),
-                form -> form.getEducation().getSchool().getLocation(),
-                form -> form.getEducation().getGraduationTypeToString(),
-                form -> form.getEducation().getSchool().getName(),
-                form -> form.getEducation().getSchool().getCode(),
-                form -> form.getGrade().getSubjectList().getSubjectMap().getSubjectListOf(2, 1).size(),
-                form -> form.getGrade().getSubjectList().getSubjectMap().getSubjectListOf(2, 1).totalScore(),
-                form -> form.getGrade().getSubjectList().getSubjectMap().getSubjectListOf(2, 2).size(),
-                form -> form.getGrade().getSubjectList().getSubjectMap().getSubjectListOf(2, 2).totalScore(),
-                form -> form.getGrade().getSubjectList().getSubjectMap().getSubjectListOf(3, 1).size(),
-                form -> form.getGrade().getSubjectList().getSubjectMap().getSubjectListOf(3, 1).totalScore(),
-                form -> MathUtil.roundTo(form.getScore().getSubjectGradeScore(), 3),
-                form -> form.getScore().getAttendanceScore(),
-                form -> form.getScore().getVolunteerScore(),
-                form -> form.getScore().getBonusScore(),
-                form -> form.getScore().getDepthInterviewScore(),
-                form -> form.getScore().getNcsScore(),
-                form -> form.getScore().getCodingTestScore(),
-                form -> form.tookSecondRound() ? MathUtil.roundTo(form.getScore().getTotalScore(), 3) : MathUtil.roundTo(form.getScore().getFirstRoundScore(), 3)
+        List<FormColumn> columnList = List.of(
+                FormColumn.text(Form::getId),
+                FormColumn.text(Form::getExaminationNumber),
+                FormColumn.text(form -> form.getOriginalType().getDescription()),
+                FormColumn.text(form -> form.getType().getDescription()),
+                FormColumn.text(form -> form.getStatus().getDescription()),
+                FormColumn.text(form -> form.getApplicant().getName()),
+                FormColumn.text(form -> form.getApplicant().getGender().getDescription()),
+                FormColumn.text(form -> form.getApplicant().getBirthday().format(DateTimeFormatter.BASIC_ISO_DATE)),
+                FormColumn.text(form -> form.getEducation().getSchool().getLocation()),
+                FormColumn.text(form -> form.getEducation().getGraduationTypeToString()),
+                FormColumn.text(form -> form.getEducation().getSchool().getName()),
+                FormColumn.text(form -> form.getEducation().getSchool().getCode()),
+                FormColumn.text(form -> form.getGrade().getSubjectList().getSubjectMap().getSubjectListOf(2, 1).size()),
+                FormColumn.text(form -> form.getGrade().getSubjectList().getSubjectMap().getSubjectListOf(2, 1).totalScore()),
+                FormColumn.text(form -> form.getGrade().getSubjectList().getSubjectMap().getSubjectListOf(2, 2).size()),
+                FormColumn.text(form -> form.getGrade().getSubjectList().getSubjectMap().getSubjectListOf(2, 2).totalScore()),
+                FormColumn.text(form -> form.getGrade().getSubjectList().getSubjectMap().getSubjectListOf(3, 1).size()),
+                FormColumn.text(form -> form.getGrade().getSubjectList().getSubjectMap().getSubjectListOf(3, 1).totalScore()),
+                FormColumn.score(form -> MathUtil.roundTo(form.getScore().getSubjectGradeScore(), 3)),
+                FormColumn.score(form -> form.getScore().getAttendanceScore()),
+                FormColumn.score(form -> form.getScore().getVolunteerScore()),
+                FormColumn.score(form -> form.getScore().getBonusScore()),
+                FormColumn.score(form -> form.getScore().getDepthInterviewScore()),
+                FormColumn.score(form -> form.getScore().getNcsScore()),
+                FormColumn.score(form -> form.getScore().getCodingTestScore()),
+                FormColumn.score(form -> form.tookSecondRound() ? MathUtil.roundTo(form.getScore().getTotalScore(), 3) : MathUtil.roundTo(form.getScore().getFirstRoundScore(), 3))
         );
 
-        List<String> styleList = List.of(
-                "default",
-                "default",
-                "default",
-                "default",
-                "default",
-                "default",
-                "default",
-                "default",
-                "default",
-                "default",
-                "default",
-                "default",
-                "default",
-                "default",
-                "default",
-                "default",
-                "default",
-                "default",
-                "default",
-                "right",
-                "right",
-                "right",
-                "right",
-                "right",
-                "right",
-                "right",
-                "right"
-        );
-
-        return xlsxGenerator.execute("전체결과", formList, columnList, styleList);
+        return xlsxGenerator.execute("전체결과", formList, columnList);
     }
 }
